e3e7bc9ca97c707834784d464e643b1b8c1b51dc
Traap <gary.a.howard@mac.com>
2021-05-31 21:41:16 -0500

    Apply EasyAling to nvim-web-devicons.

--- lua/config/nvim-web-devicons.lua
+++ lua/config/nvim-web-devicons.lua
@@ -1,109 +1,28 @@
+-- Setup icons with nvim-web-devicons.
+
 require "nvim-web-devicons".setup {
     override = {
-        html = {
-            icon = "",
-            color = "#DE8C92",
-            name = "html"
-        },
-        css = {
-            icon = "",
-            color = "#61afef",
-            name = "css"
-        },
-        js = {
-            icon = "",
-            color = "#EBCB8B",
-            name = "js"
-        },
-        ts = {
-            icon = "ﯤ",
-            color = "#519ABA",
-            name = "ts"
-        },
-        kt = {
-            icon = "󱈙",
-            color = "#ffcb91",
-            name = "kt"
-        },
-        png = {
-            icon = " ",
-            color = "#BD77DC",
-            name = "png"
-        },
-        jpg = {
-            icon = " ",
-            color = "#BD77DC",
-            name = "jpg"
-        },
-        jpeg = {
-            icon = " ",
-            color = "#BD77DC",
-            name = "jpeg"
-        },
-        mp3 = {
-            icon = "",
-            color = "#C8CCD4",
-            name = "mp3"
-        },
-        mp4 = {
-            icon = "",
-            color = "#C8CCD4",
-            name = "mp4"
-        },
-        out = {
-            icon = "",
-            color = "#C8CCD4",
-            name = "out"
-        },
-        Dockerfile = {
-            icon = "",
-            color = "#b8b5ff",
-            name = "Dockerfile"
-        },
-        rb = {
-            icon = "",
-            color = "#ff75a0",
-            name = "rb"
-        },
-        vue = {
-            icon = "﵂",
-            color = "#7eca9c",
-            name = "vue"
-        },
-        py = {
-            icon = "",
-            color = "#a7c5eb",
-            name = "py"
-        },
-        toml = {
-            icon = "",
-            color = "#61afef",
-            name = "toml"
-        },
-        lock = {
-            icon = "",
-            color = "#DE6B74",
-            name = "lock"
-        },
-        zip = {
-            icon = "",
-            color = "#EBCB8B",
-            name = "zip"
-        },
-        xz = {
-            icon = "",
-            color = "#EBCB8B",
-            name = "xz"
-        },
-        deb = {
-            icon = "",
-            color = "#a3b8ef",
-            name = "deb"
-        },
-        rpm = {
-            icon = "",
-            color = "#fca2aa",
-            name = "rpm"
-        }
-    }
+        Dockerfile = { icon = "",  color = "#b8b5ff", name = "Dockerfile" },
+        css        = { icon = "",  color = "#61afef", name = "css" },
+        deb        = { icon = "",  color = "#a3b8ef", name = "deb" },
+        html       = { icon = "",  color = "#DE8C92", name = "html" },
+        jpeg       = { icon = " ", color = "#BD77DC", name = "jpeg" },
+        jpg        = { icon = " ", color = "#BD77DC", name = "jpg" },
+        js         = { icon = "",  color = "#EBCB8B", name = "js" },
+        kt         = { icon = "󱈙",  color = "#ffcb91", name = "kt" },
+        lock       = { icon = "",  color = "#DE6B74", name = "lock" },
+        mp3        = { icon = "",  color = "#C8CCD4", name = "mp3" },
+        mp4        = { icon = "",  color = "#C8CCD4", name = "mp4" },
+        out        = { icon = "",  color = "#C8CCD4", name = "out" },
+        png        = { icon = " ", color = "#BD77DC", name = "png" },
+        py         = { icon = "",  color = "#a7c5eb", name = "py" },
+        rb         = { icon = "",  color = "#ff75a0", name = "rb" },
+        rpm        = { icon = "",  color = "#fca2aa", name = "rpm" },
+        toml       = { icon = "",  color = "#61afef", name = "toml" },
+        ts         = { icon = "ﯤ",  color = "#519ABA", name = "ts" },
+        vue        = { icon = "﵂",  color = "#7eca9c", name = "vue" },
+        xz         = { icon = "",  color = "#EBCB8B", name = "xz" },
+        yaml       = { icon = "",  color = "#EBCB8B", name = "xz" },
+        zip        = { icon = "",  color = "#EBCB8B", name = "zip" }
+    };
 }
