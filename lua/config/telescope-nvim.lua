17333a097a39eb52e3b525703c9310dee1866091
Traap <gary.a.howard@mac.com>
2021-06-03 22:51:21 -0500

    Add pdf to telescope media viewer.

--- lua/config/telescope-nvim.lua
+++ lua/config/telescope-nvim.lua
@@ -48,7 +48,7 @@ require("telescope").setup {
     },
     extensions = {
         media_files = {
-            filetypes = {"png", "webp", "jpg", "jpeg"},
+            filetypes = {"pdf", "png", "webp", "jpg", "jpeg"},
             find_cmd = "rg" -- find command (defaults to `fd`)
         }
     }
