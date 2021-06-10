4ea04c70d73cbaddea109cf1ae4543f934d049c8
Traap <gary.a.howard@mac.com>
2021-06-09 23:24:41 -0500

    Apply my personal comment / fold style.

--- lua/traap/keybindings.lua
+++ lua/traap/keybindings.lua
@@ -21,12 +21,13 @@ end
 g.maplocalleader = [[ ]]
 
 -- ------------------------------------------------------------------------- }}}
--- {{{ Keybindings
+-- {{{ Stop search highlights.
 
--- Stop search highlights.
 map('n', '<leader><space>', '<cmd>noh<CR>')
 
--- Find files using Telescope command-line suggar.
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Find files using Telescope command-line suggar.
+
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
@@ -34,19 +35,26 @@ map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 map('n', '<leader>fo', '<cmd>Telescope oldfiles<cr>')
 map('n', '<leader>mf', '<cmd>Telescope media_files<cr>')
 
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Buffer resize
 
--- Buffer resize
 map('n', '<a-H>', '<cmd>vertical resize -1<cr>')
 map('n', '<a-J>', '<cmd>resize +1<cr>')
 map('n', '<a-K>', '<cmd>resize -1<cr>')
 map('n', '<a-L>', '<cmd>vertical resize +1<cr>')
 
--- Copy and Paste
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Copy and Paste
+
 map('n', '<leader>cc', 'ggVGg_"+y')
 map('v', '<leader>cc', '"+y')
 map('n', '<leader>cv', '"+p')
 
--- Make only the current buffer visible.
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Buffer selecdtions
+
+-- BuMake only the current buffer visible.
+
 map('n', '<leader>oo', '<cmd>only<cr>')
 
 -- Delete the current line.
@@ -62,21 +70,29 @@ map('n', 'Vaa', 'ggvG')
 -- Linewise reslection of what you just pasted.
 map('n', '<leader>V', 'V`')
 
--- Window movement without Tmux-Navigator
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Window movement without Tmux-Navigator
+
 map('n', '<c-h>', '<c-w>h')
 map('n', '<c-j>', '<c-w>j')
 map('n', '<c-k>', '<c-w>k')
 map('n', '<c-l>', '<c-w>l')
 
--- Clean trailing whitespaces
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Clean trailing whitespaces
+
 map('n', '<leader>wr', '<cmd>%s/\r//g<cr>')
 map('n', '<leader>ws', "mz<cmd>%s//\\s\\+$//<cr><cmd>let @/=''<cr>`z")
 
--- KJV verse lookup.
+-- ------------------------------------------------------------------------- }}}
+-- {{{ KJV verse lookup.
+
 map('n', 'gk', [[0mMvg_"ky<cmd>exec 'r!kjv -b -d -w 65' getreg('k')<cr>]])
 map('v', 'gs', 'S*v)3>')
 
--- Fugitive
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Fugitive
+
 map('n', '<Leader>gc', '<cmd>G commit<CR>')
 map('n', '<Leader>gd', '<cmd>G diff<CR>')
 map('n', '<Leader>gh', '<cmd>silent vert bo help fugitive<cr>')
@@ -85,11 +101,15 @@ map('n', '<Leader>gP', '<cmd>G pull<CR>')
 map('n', '<Leader>gp', '<cmd>G push<CR>')
 map('n', '<Leader>gs', '<cmd>G<CR>')
 
--- <Tab> to navigate the completion menu
+-- ------------------------------------------------------------------------- }}}
+-- {{{ <Tab> to navigate the completion menu
+
 map('n', '<S-Tab>', 'pumvisible() ? [[\\<C-p>" : "\\<Tab>"', {expr = true})
 map('n', '<Tab>',   'pumvisible() ? "\\<C-n>" : "\\<Tab>"',  {expr = true})
 
--- nvim-tree
+-- ------------------------------------------------------------------------- }}}
+-- {{{ nvim-tree
+
 map('n', '<c-n>',       '<cmd>NvimTreeToggle<cr>')
 map('n', '<leader>nr',  '<cmd>NvimTreeRefresh<cr>')
 map('n', '<leader>nf',  '<cmd>NvimTreeFindFile<cr>')
@@ -104,11 +124,15 @@ g.nvim_tree_bindings = {
   ['q']      = tree_cb('close'),
 }
 
--- wiki.vim
+-- ------------------------------------------------------------------------- }}}
+-- {{{ wiki.vim
+
 map('n', '<leader>fw', '<cmd>WikiFzfPages<cr>')
 
--- Easy align
-vim.cmd([[
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Easy align
+
+cmd([[
   xmap ga <Plug>(EasyAlign)
   nmap ga <Plug>(EasyAlign)
   nmap <bar> gaip*<bar>
