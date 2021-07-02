5b3907a5d3da373e8c50d77469af0a3154936fcd
Traap <gary.a.howard@mac.com>
2021-07-01 20:48:02 -0500

    Address Telescope deprecations.

--- lua/traap/telescope-nvim.lua
+++ lua/traap/telescope-nvim.lua
@@ -9,7 +9,6 @@ require('telescope').setup {
       '--column',
       '--smart-case'
     },
-    prompt_position = 'bottom',
     prompt_prefix = ' ',
     selection_caret = ' ',
     entry_prefix = '  ',
@@ -17,24 +16,21 @@ require('telescope').setup {
     selection_strategy = 'reset',
     sorting_strategy = 'descending',
     layout_strategy = 'horizontal',
-    layout_defaults = {
+    layout_config = {
       horizontal = {
         mirror = false,
         preview_width = 0.5
       },
       vertical = {
         mirror = false
-      }
+      },
+      width = 0.75
     },
     file_sorter = require 'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter = require 'telescope.sorters'.get_generic_fuzzy_sorter,
     shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
     border = {},
     borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
     color_devicons = true,
