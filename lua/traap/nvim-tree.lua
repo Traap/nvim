bfc47fbd33638b0f8ae5d30a3a97f00384610569
Traap <gary.a.howard@mac.com>
2021-07-01 20:39:30 -0500

    Adjust for deprecated NvimTree keybindings.

--- lua/traap/nvim-tree.lua
+++ lua/traap/nvim-tree.lua
@@ -1,11 +1,15 @@
--- nvim-tree configuration
---
--- help NvimTree to review defaults.
+-- {{{ Alias to vim APis.
 
 local g = vim.g
 
--- 
+-- ------------------------------------------------------------------------- }}}
+-- {{{ nvim-tree configuration
+
+-- help NvimTree to review defaults.
+
+g.nvim_tree_quit_on_open = 1
 g.nvim_tree_disable_window_picker = 1
+g.nvim_tree_follow = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_gitignore = 1
 g.nvim_tree_hide_dotfiles = 1
@@ -14,3 +18,5 @@ g.nvim_tree_indent_markers = 1
 g.nvim_tree_side = 'right'
 g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
 g.nvim_tree_width = 27
+
+-- ------------------------------------------------------------------------- }}}
