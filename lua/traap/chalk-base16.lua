096b3dfcde2f5f67cf5a5330c90977fa4628ec47
Traap <gary.a.howard@mac.com>
2021-08-20 09:40:13 -0500

    Add copy of chalk-base16.lua

---
+++ lua/traap/chalk-base16.lua
@@ -0,0 +1,18 @@
+return {
+    base0A = "ddb26f",
+    base04 = "b0b0b0",
+    base07 = "f5f5f5",
+    base05 = "d0d0d0",
+    base0E = "e1a3ee",
+    base0D = "6fc2ef",
+    base0C = "12cfc0",
+    base0B = "acc267",
+    base02 = "303030",
+    base0F = "deaf8f",
+    base03 = "505050",
+    base08 = "fb9fb1",
+    base01 = "202020",
+    base00 = "151515",
+    base09 = "eda987",
+    base06 = "e0e0e0"
+}
