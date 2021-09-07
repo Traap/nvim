91ec705fe834fcc1c6f932097c6e9837b8a3b274
Traap <gary.a.howard@mac.com>
2021-09-07 10:38:50 -0500

    Add nvim-tree customization options.

--- lua/traap/nvim-tree.lua
+++ lua/traap/nvim-tree.lua
@@ -7,14 +7,17 @@ local g = vim.g
 
 -- help NvimTree to review defaults.
 
-g.nvim_tree_quit_on_open = 1
+g.nvim_tree_disable_netrw = 0
 g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_follow = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_gitignore = 1
 g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_highlight_opened_files = 3
+g.nvim_tree_hijack_netrw = 0
 g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
 g.nvim_tree_indent_markers = 1
+g.nvim_tree_quit_on_open = 1
 g.nvim_tree_side = 'right'
 g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
 g.nvim_tree_width = 27
