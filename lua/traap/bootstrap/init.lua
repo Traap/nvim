bc8201c87d6ec012532662a41aa713e4fda6afe5
Traap <gary.a.howard@mac.com>
2021-12-22 08:31:30 -0600

    Always packadd packer.nvim

--- lua/traap/bootstrap/init.lua
+++ lua/traap/bootstrap/init.lua
@@ -8,14 +8,13 @@ local url = 'https://github.com/wbthomason/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   print('Installing packer.')
   vim.fn.system({'git', 'clone', url, install_path})
-
-  -- Add packer.
-  vim.cmd [[packadd packer.nvim]]
-
   vim.g.nvim_bootstrapped = 1
 else
   vim.g.nvim_bootstrapped = 0
 end
+
+-- Add packer.
+vim.cmd [[packadd packer.nvim]]
 
 -- Auto compile when there are changes in plugins.lua
 vim.cmd [[
