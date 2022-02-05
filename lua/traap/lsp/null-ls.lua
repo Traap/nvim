e281cc8ce39742d8a611660ceb457c340a26d564
Traap <gary.a.howard@mac.com>
2022-02-04 22:49:35 -0600

    Remove null_ls features.

--- lua/traap/lsp/null-ls.lua
+++ lua/traap/lsp/null-ls.lua
@@ -9,8 +9,12 @@ if not null_ls_ok then return end
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.completion.spell,
-  },
+    -- null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.write_good,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.rubocop,
+  }
 })
