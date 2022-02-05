664025210242d90a35cde125f02ffa90429fc116
Traap <gary.a.howard@mac.com>
2022-02-05 00:55:54 -0600

    Change keys again.

--- lua/traap/lsp/handlers.lua
+++ lua/traap/lsp/handlers.lua
@@ -81,22 +81,20 @@ end
 local function lsp_keymaps(bufnr)
   local opts = {noremap = true, silent = true}
   local keymap = vim.api.nvim_buf_set_keymap
-  local buf = vim.lsp.buf
-  local diag = vim.diagnostic
 
-  keymap(bufnr, 'n', '<leader>ca', [[<cmd>lua buf.code_action()<CR>]],                                opts)
-  keymap(bufnr, 'n', '<leader>f',  [[<cmd>lua diag.open_float()<CR>]],                                opts)
-  keymap(bufnr, 'n', '<leader>k',  [[<cmd>lua buf.signature_help()<CR>]],                             opts)
-  keymap(bufnr, 'n', '<leader>q',  [[<cmd>lua diag.setloclist()<CR>]],                                opts)
-  keymap(bufnr, 'n', '<leader>rn', [[<cmd>lua buf.rename()<CR>]],                                     opts)
-  keymap(bufnr, 'n', 'K',          [[<cmd>lua buf.hover()<CR>]],                                      opts)
-  keymap(bufnr, 'n', 'gD',         [[<cmd>lua buf.declaration()<CR>]],                                opts)
-  keymap(bufnr, 'n', 'gd',         [[<cmd>lua buf.definition()<CR>]],                                 opts)
-  keymap(bufnr, 'n', 'gi',         [[<cmd>lua buf.implementation()<CR>]],                             opts)
-  keymap(bufnr, 'n', 'gl',         [[<cmd>lua diag.show_line_diagnostics({border = 'rounded'})<CR>]], opts)
-  keymap(bufnr, 'n', '[d',         [[<cmd>lua diag.goto_prev({border = 'rounded' })<CR>]],            opts)
-  keymap(bufnr, 'n', ']d',         [[<cmd>lua diag.goto_next({border = 'rounded' })<CR>]],            opts)
-  keymap(bufnr, 'n', 'gr',         [[<cmd>lua buf.references()<CR>]],                                 opts)
+  keymap(bufnr, 'n', '<leader>ca', [[<cmd>lua vim.lsp.buf.code_action()<CR>]],                                  opts)
+  keymap(bufnr, 'n', '<leader>f',  [[<cmd>lua vim.diagnostic.open_float()<CR>]],                                opts)
+  keymap(bufnr, 'n', '<leader>k',  [[<cmd>lua vim.lsp.buf.signature_help()<CR>]],                               opts)
+  keymap(bufnr, 'n', '<leader>q',  [[<cmd>lua vim.diagnostic.setloclist()<CR>]],                                opts)
+  keymap(bufnr, 'n', '<leader>rn', [[<cmd>lua vim.lsp.buf.rename()<CR>]],                                       opts)
+  keymap(bufnr, 'n', 'K',          [[<cmd>lua vim.lsp.buf.hover()<CR>]],                                        opts)
+  keymap(bufnr, 'n', 'gD',         [[<cmd>lua vim.lsp.buf.declaration()<CR>]],                                  opts)
+  keymap(bufnr, 'n', 'gd',         [[<cmd>lua vim.lsp.buf.definition()<CR>]],                                   opts)
+  keymap(bufnr, 'n', 'gi',         [[<cmd>lua vim.lsp.buf.implementation()<CR>]],                               opts)
+  keymap(bufnr, 'n', 'gl',         [[<cmd>lua vim.diagnostic.show_line_diagnostics({border = 'rounded'})<CR>]], opts)
+  keymap(bufnr, 'n', '[d',         [[<cmd>lua vim.diagnostic.goto_prev({border = 'rounded' })<CR>]],            opts)
+  keymap(bufnr, 'n', ']d',         [[<cmd>lua vim.diagnostic.goto_next({border = 'rounded' })<CR>]],            opts)
+  keymap(bufnr, 'n', 'gr',         [[<cmd>lua vim.lsp.buf.references()<CR>]],                                   opts)
 
   vim.cmd [[command! Format execute 'lua vim.lsp.buf.formatting()']]
 end
