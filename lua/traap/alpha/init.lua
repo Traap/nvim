2199af794f608d9997c42870eb57b43376a97fa3
Traap <gary.a.howard@mac.com>
2022-07-25 07:37:09 -0500

    Expand bspwm bindings.

--- lua/traap/alpha/init.lua
+++ lua/traap/alpha/init.lua
@@ -26,15 +26,16 @@ dashboard.section.buttons.val = {
   dashboard.button("c", "  Configuration",       ":e ~/.config/nvim/init.lua <CR>"),
   dashboard.button("e", "  New file",            ":ene <BAR> startinsert <CR>"),
   dashboard.button("f", "  Find file",           ":Telescope find_files <CR>"),
+  dashboard.button("k", "  Keybindings",         ":e~/git/nvim/lua/traap/keybindings/init.lua<CR>"),
   dashboard.button("m", "  Monitors",            ":e ~/.config/bspwm/bspwm-monitor <CR>"),
   dashboard.button("n", "  Notebook",            ":e ~/git/wiki/index.md<CR>"),
   dashboard.button("p", "  Plugins",             ":e ~/.config/nvim/lua/traap/plugins/init.lua <CR>"),
+  dashboard.button("q", "  Quit Neovim",         ":qa<CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("s", "  sxhkd",               ":e ~/.config/bspwm/sxhkd/sxhkdrc <CR>"),
   dashboard.button("t", "  Find text",           ":Telescope live_grep <CR>"),
-  dashboard.button("k", "  Keybindings",         ":e~/git/nvim/lua/traap/keybindings/init.lua<CR>"),
-  dashboard.button("w", "  WhichKey",            ":e~/git/nvim/lua/traap/which-key/init.lua<CR>"),
-  dashboard.button("q", "  Quit Neovim",         ":qa<CR>"),
+  dashboard.button("v", "  Resume",              ":e ~/git/wiki/personal/cv/cv.tex<CR>"),
+  dashboard.button("w", "  WhichKey",            ":e ~/git/nvim/lua/traap/which-key/init.lua<CR>"),
 }
 
 local function footer()
