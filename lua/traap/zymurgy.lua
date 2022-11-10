13b09e0e02f87e10942fe88a3390fc4436f53920
Traap <gary.a.howard@mac.com>
2022-11-10 12:56:52 -0800

    Experiment wtih copy & paste lua style.

--- lua/traap/zymurgy.lua
+++ lua/traap/zymurgy.lua
@@ -52,3 +52,15 @@ end
 
 vim.g.transparent_enable = true
 vim.api.nvim_create_autocmd('ColorScheme', { command = 'highlight clear Folded'})
+
+-- https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html See
+in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = 'wsl clipboard',
+    copy =  { ["+"] = { "clip.exe" },   ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true
+  }
+end
