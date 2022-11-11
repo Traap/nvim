51c341f85cfb1d3b2d6a6ba7d06aabb9b3183346
Traap <gary.a.howard@mac.com>
2022-11-11 14:28:09 -0800

    Experiment with treesitter text objects.

--- lua/traap/treesitter.lua
+++ lua/traap/treesitter.lua
@@ -31,62 +31,69 @@ local disable = {
   'markdown',
 }
 
+-- config.setup {
+--   autopairs = {enable = true,},
+--   autotag = {
+--     enable = true,
+--     disable = { "xml" },
+--   },
+--   context_commenting = {
+--     enable = true,
+--     enable_autocmd = false,
+--   },
+--   ensure_installed = languages,
+--   highlight = {
+--     enable = true,
+--     disable = disable,
+--     additional_vim_regex_highlighting=true,
+--   },
+--   ignore_install = {''},
+--   indent = {enable = true, disable = {'yaml'}},
+--   playground = { enable = true, },
+--   rainbow = {
+--     colors = {
+--       "Gold",
+--       "Orchid",
+--       "DodgerBlue",
+--       "Cornsilk",
+--       "Salmon",
+--       "LawnGreen",
+--     },
+--     disable = { "html" },
+--     enable = true,
+--     extended_mode = true,
+--   },
+--   refactor = {
+--     highlight_current_scope = { enable = true },
+--     highlight_definitions = {
+--       enable = true,
+--       clear_on_cursor_move = true,
+--     },
+--     navigation = {
+--       enable = true,
+--       keymaps = {
+--         goto_definition = "gnd",
+--         list_definitions = "gnD",
+--         list_definitions_toc = "gO",
+--         goto_next_usage = "<a-*>",
+--         goto_previous_usage = "<a-#>",
+--       },
+--     },
+--     smart_rename = {
+--       enable = true,
+--       keymaps = {
+--         smart_rename = "grr",
+--       },
+--     },
+--   },
+--   sync_install = false,
+-- }
+
 config.setup {
-  autopairs = {enable = true,},
-  autotag = {
-    enable = true,
-    disable = { "xml" },
-  },
-  context_commenting = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  ensure_installed = languages,
   highlight = {
     enable = true,
-    disable = disable,
     additional_vim_regex_highlighting=true,
   },
-  ignore_install = {''},
-  indent = {enable = true, disable = {'yaml'}},
-  playground = { enable = true, },
-  rainbow = {
-    colors = {
-      "Gold",
-      "Orchid",
-      "DodgerBlue",
-      "Cornsilk",
-      "Salmon",
-      "LawnGreen",
-    },
-    disable = { "html" },
-    enable = true,
-    extended_mode = true,
-  },
-  refactor = {
-    highlight_current_scope = { enable = true },
-    highlight_definitions = {
-      enable = true,
-      clear_on_cursor_move = true,
-    },
-    navigation = {
-      enable = true,
-      keymaps = {
-        goto_definition = "gnd",
-        list_definitions = "gnD",
-        list_definitions_toc = "gO",
-        goto_next_usage = "<a-*>",
-        goto_previous_usage = "<a-#>",
-      },
-    },
-    smart_rename = {
-      enable = true,
-      keymaps = {
-        smart_rename = "grr",
-      },
-    },
-  },
-  sync_install = false,
 }
 
 -- ------------------------------------------------------------------------- }}}
