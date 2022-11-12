35838da7e933140751e88ba4fce6eadbe776201c
Traap <gary.a.howard@mac.com>
2022-11-12 10:17:56 -0800

    Configure null_ls formatting.

--- lua/traap/lsp/null-ls.lua
+++ lua/traap/lsp/null-ls.lua
@@ -1,12 +1,16 @@
-local  null_ls_ok, null_ls = pcall(require, 'null-ls')
-if not null_ls_ok then return end
+local  ok, null_ls = pcall(require, 'null-ls')
+if not ok then return end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
+  debug = false,
   sources = {
     null_ls.builtins.completion.spell,
-    -- null_ls.builtins.diagnostics.write_good,
-    -- null_ls.builtins.formatting.prettier,
-    -- null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.rubocop,
+    formatting.prettier.with({ extra_args = { "--no-simi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
   }
 })
