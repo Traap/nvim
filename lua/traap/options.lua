a0f3e5abbc0e8328448ef151758d4fc7cbcf795f
Traap <gary.a.howard@mac.com>
2022-11-12 08:22:37 -0800

    Add options to a lua table.

--- lua/traap/options.lua
+++ lua/traap/options.lua
@@ -1,64 +1,57 @@
--- {{{ Alias to vim APis.
-
-local g = vim.g
-local cmd = vim.cmd
-local opt = vim.opt
-
--- ------------------------------------------------------------------------- }}}
 -- {{{ Alphabetical listing of settings I like.
 
--- opt.background = "dark"
-opt.clipboard = 'unnamedplus'
-opt.colorcolumn = '+1'
-opt.complete = { '.', 'w', 'b', 'u', 't', 'i', 'kspell' }
-opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
-opt.cursorline = false
-opt.expandtab = true
-opt.fillchars = 'fold: '
-opt.foldlevel = 20
-opt.foldmethod = 'marker'
-opt.hidden = true
-opt.ignorecase = true
-opt.lazyredraw = true
-opt.listchars = { eol = '↲', tab = '▸ ', trail = '·' }
-opt.mouse = 'a'
-opt.nrformats = { 'alpha', 'octal', 'hex' }
-opt.number = true
-opt.numberwidth = 3
-opt.relativenumber = true
-opt.scrolloff = 4
-opt.shiftround = true
-opt.shiftwidth = 2
-opt.shortmess = 'aToOS'
-opt.showbreak = '↪'
-opt.showmatch = true
-opt.showmode = false
-opt.signcolumn = 'yes'
-opt.smartcase = true
-opt.smartindent = true
-opt.softtabstop = 2
-opt.splitbelow = true
-opt.splitright = true
-opt.swapfile = false
-opt.termguicolors = true
-opt.timeoutlen = 500
-opt.textwidth = 80
-opt.virtualedit = { 'block' }
-opt.wildmode = { 'list', 'longest' }
-opt.wrap = false
-cmd([[filetype plugin indent on]])
-cmd([[syntax enable]])
 
--- ------------------------------------------------------------------------- }}}
--- {{{ Completion confirmation.
+local options = {
+  clipboard = 'unnamedplus',
+  colorcolumn = '+1',
+  complete = { '.', 'w', 'b', 'u', 't', 'i', 'kspell' },
+  completeopt = { 'menuone', 'noinsert', 'noselect' },
+  cursorline = false,
+  expandtab = true,
+  fillchars = 'fold: ',
+  foldlevel = 20,
+  foldmethod = 'marker',
+  hidden = true,
+  ignorecase = true,
+  lazyredraw = true,
+  listchars = { eol = '↲', tab = '▸ ', trail = '·' },
+  mouse = 'a',
+  nrformats = { 'alpha', 'octal', 'hex' },
+  number = true,
+  numberwidth = 3,
+  relativenumber = true,
+  scrolloff = 4,
+  shiftround = true,
+  shiftwidth = 2,
+  shortmess = 'aToOS',
+  showbreak = '↪',
+  showmatch = true,
+  showmode = false,
+  signcolumn = 'yes',
+  smartcase = true,
+  smartindent = true,
+  softtabstop = 2,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  termguicolors = true,
+  timeoutlen = 500,
+  textwidth = 80,
+  virtualedit = { 'block' },
+  wildmode = { 'list', 'longest' },
+  wrap = false,
+}
 
--- g.completion_confirm_key = ""
--- g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+for k,v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd([[filetype plugin indent on]])
 
 -- ------------------------------------------------------------------------- }}}
 -- {{{ Highlight on yank
 
-cmd([[
+vim.cmd([[
   augroup YankGroup
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank{on_visual = false}
@@ -68,7 +61,7 @@ cmd([[
 -- ------------------------------------------------------------------------- }}}
 -- {{{ Bullets
 
-g.bullets_enabled_file_types = {
+vim.g.bullets_enabled_file_types = {
   'gitcommit',
   'markdown',
   'scratch',
