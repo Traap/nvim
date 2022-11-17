527c9bf143a85dcfee17ce353565a345551a0a9e
Traap <gary.a.howard@mac.com>
2022-11-17 08:00:39 -0800

    Add normal and visual mode options for future use.

--- lua/traap/config.lua
+++ lua/traap/config.lua
@@ -7,59 +7,59 @@ User = {
 -- ------------------------------------------------------------------------- }}}
 -- {{{ language server names.
 
-  use_lsp = true,
-  language_servers = {
-    'bashls',
-    'cssls',
-    'diagnosticls',
-    'html',
-    'jsonls',
-    'emmet_ls',
-    'pyright',
-    -- 'rust',
-    'texlab',
-    -- 'tsserver',
-    'solargraph',
-    'yamlls',
-    'sumneko_lua',
-  },
+use_lsp = true,
+language_servers = {
+  'bashls',
+  'cssls',
+  'diagnosticls',
+  'html',
+  'jsonls',
+  'emmet_ls',
+  'pyright',
+  -- 'rust',
+  'texlab',
+  -- 'tsserver',
+  'solargraph',
+  'yamlls',
+  'sumneko_lua',
+},
 
 -- ------------------------------------------------------------------------- }}}
 -- {{{ Lsp kind icons.
 
-  lsp_kind_icons = {
-    Class         = "",
-    Color         = "",
-    Constant      = "",
-    Constructor   = "",
-    Enum          = "",
-    EnumMember    = "",
-    Event         = "",
-    Field         = "",
-    File          = "",
-    Folder        = "",
-    Function      = "",
-    Interface     = "",
-    Keyword       = "",
-    Method        = "",
-    Module        = "",
-    Operator      = "",
-    Property      = "",
-    Reference     = "",
-    Snippet       = " ",
-    Struct        = "",
-    Text          = "",
-    TypeParameter = "",
-    Unit          = "",
-    Value         = "",
-    Variable      = "",
-    buffer        = "﬘ ",
-    calc          = " ",
-    nvim_lsp      = " ",
-    path          = " ",
-    spell         = " ",
-    vsnip         = " ",
-  },
+lsp_kind_icons = {
+  Class         = "",
+  Color         = "",
+  Constant      = "",
+  Constructor   = "",
+  Enum          = "",
+  EnumMember    = "",
+  Event         = "",
+  Field         = "",
+  File          = "",
+  Folder        = "",
+  Function      = "",
+  Interface     = "",
+  Keyword       = "",
+  Method        = "",
+  Module        = "",
+  Operator      = "",
+  Property      = "",
+  Reference     = "",
+  Snippet       = " ",
+  Struct        = "",
+  Text          = "",
+  TypeParameter = "",
+  Unit          = "",
+  Value         = "",
+  Variable      = "",
+  buffer        = "﬘ ",
+  calc          = " ",
+  nvim_lsp      = " ",
+  path          = " ",
+  spell         = " ",
+  vsnip         = " ",
+},
 
 -- ------------------------------------------------------------------------- }}}
 -- {{{ Diagnostics Signs.
@@ -75,6 +75,29 @@ User = {
 -- {{{ Display boarders
 
 display_border = {border = {'┌', '─', '┐', '│', '┘', '─', '└', '│'}},
+
+-- ------------------------------------------------------------------------- }}}
+-- {{{ keybinding options
+
+normal_opts = {
+  mode    = "n",        -- NORMAL mode
+  prefix  = "<leader>", -- Override this value.
+  buffer  = nil,        -- Global mappings. Specify a buffer number for buffer
+                        --   local mappings
+  silent  = true,       -- use `silent` when creating keymaps
+  noremap = true,       -- use `noremap` when creating keymaps
+  nowait  = true,       -- use `nowait` when creating keymaps
+},
+
+visual_opts = {
+  mode    = "v",        -- VISUAL mode
+  prefix  = "<leader>", -- Override this value.
+  buffer  = nil,        -- Global mappings. Specify a buffer number for buffer
+                        --   local mappings
+  silent  = true,       -- use `silent` when creating keymaps
+  noremap = true,       -- use `noremap` when creating keymaps
+  nowait  = true,       -- use `nowait` when creating keymaps
+},
 
 -- ------------------------------------------------------------------------- }}}
 -- {{{ Return User table.
