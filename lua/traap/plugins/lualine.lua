3c269c9562ae96f28470eb693b87f607da74aab6
Traap <gary.a.howard@mac.com>
2023-01-15 22:54:00 -0800

    Remove lualine.lua and notify.lua

--- lua/traap/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
--- {{{ Requires and aliases
-
-local  ll_ok, ll = pcall(require, 'lualine')
-if not ll_ok then return end
-
--- ------------------------------------------------------------------------- }}}
--- {{{
-
-ll.setup()
