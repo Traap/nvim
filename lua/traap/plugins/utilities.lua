356a4a7dc68800e5df13be3fb26358e013ad745a
Traap <gary.a.howard@mac.com>
2023-01-19 00:37:29 -0800

    Move treesitter back into utilities.lua

--- lua/traap/plugins/utilities.lua
+++ lua/traap/plugins/utilities.lua
@@ -142,6 +142,44 @@ return {
   },
 
   -- ----------------------------------------------------------------------- }}}
+  -- {{{ Treesitter
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = 1500,
+      },
+      ensure_installed = {
+        'bash',
+        'c',
+        'c_sharp',
+        'cpp',
+        'dockerfile',
+        'help',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'ruby',
+        'rust',
+        'sql',
+        'tsx',
+        'typescript',
+        'vim',
+        'yaml',
+      },
+    },
+  },
+
+  -- ----------------------------------------------------------------------- }}}
   -- {{{ Set colors
 
   { 'ChristianChiarulli/nvim-ts-rainbow', event = 'VeryLazy' },
