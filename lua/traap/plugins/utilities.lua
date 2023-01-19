0a9862308e5a23321e00041ad4bfcf313a6d239a
Traap <gary.a.howard@mac.com>
2023-01-19 00:32:25 -0800

    Move nvim-tree nvim-web-devicons back to utilities.

--- lua/traap/plugins/utilities.lua
+++ lua/traap/plugins/utilities.lua
@@ -95,6 +95,43 @@ return {
   },
 
   -------------------------------------------------------------------------- }}}
+  -- {{{ nvim-tree nvim-web-devicons
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    enabled = true,
+    event = 'VimEnter',
+    config = true,
+    opts = {
+      override = {
+        Dockerfile = {icon = '',  color = '#b8b5ff', name = 'Dockerfile'},
+        css        = {icon = '',  color = '#61afef', name = 'css'},
+        deb        = {icon = '',  color = '#a3b8ef', name = 'deb'},
+        html       = {icon = '',  color = '#DE8C92', name = 'html'},
+        jpeg       = {icon = ' ', color = '#BD77DC', name = 'jpeg'},
+        jpg        = {icon = ' ', color = '#BD77DC', name = 'jpg'},
+        js         = {icon = '',  color = '#EBCB8B', name = 'js'},
+        kt         = {icon = '󱈙',  color = '#ffcb91', name = 'kt'},
+        lock       = {icon = '',  color = '#DE6B74', name = 'lock'},
+        md         = {icon = '',  color = '#b8b5ff', name = 'mp3'},
+        mp3        = {icon = '',  color = '#C8CCD4', name = 'mp3'},
+        mp4        = {icon = '',  color = '#C8CCD4', name = 'mp4'},
+        out        = {icon = '',  color = '#C8CCD4', name = 'out'},
+        png        = {icon = ' ', color = '#BD77DC', name = 'png'},
+        py         = {icon = '',  color = '#a7c5eb', name = 'py'},
+        rb         = {icon = '',  color = '#ff75a0', name = 'rb'},
+        rpm        = {icon = '',  color = '#fca2aa', name = 'rpm'},
+        toml       = {icon = '',  color = '#61afef', name = 'toml'},
+        ts         = {icon = 'ﯤ',  color = '#519ABA', name = 'ts'},
+        vue        = {icon = '﵂',  color = '#7eca9c', name = 'vue'},
+        xz         = {icon = '',  color = '#EBCB8B', name = 'xz'},
+        yaml       = {icon = '',  color = '#EBCB8B', name = 'xz'},
+        zip        = {icon = '',  color = '#EBCB8B', name = 'zip'}
+      };
+    },
+  },
+
+  -------------------------------------------------------------------------- }}}
   -- {{{ Tmux
 
   { 'christoomey/vim-tmux-runner',
