2532cc972621c47aabafa2b5e50df254ea0b5427
Traap <gary.a.howard@mac.com>
2023-01-30 20:28:09 -0800

    Remove redundant textDocument/hover configuration.

--- lua/traap/servers/lsp/handlers.lua
+++ lua/traap/servers/lsp/handlers.lua
@@ -36,13 +36,6 @@ M.setup = function()
 
   vim.diagnostic.config(config)
 
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover, { border = 'rounded', }
-  )
-
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help, { border = 'rounded', }
-  )
 end
 
 -- ------------------------------------------------------------------------- }}}
