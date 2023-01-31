117ec85e14fe84ba052407540bdf02a5b4ef6a67
Traap <gary.a.howard@mac.com>
2023-01-31 07:54:46 -0800

    Refactor startup.
    1. Move contents of traap/core/init.lua to init.lua
    2. Move traap/keybindings/init.lua to traap/core/keybindings.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[,]]
 
-require 'traap.core'
-require 'traap.keybindings'
+require 'traap.core.bootstrap'
+require 'traap.core.clipboard'
+require 'traap.core.customize'
+require 'traap.core.functions'
+require 'traap.core.options'
+require 'traap.core.plugins'
+require 'traap.core.autocmds'
+require 'traap.core.keybindings'
--- lua/traap/core/init.lua
+++
@@ -1,7 +0,0 @@
-require 'traap.core.bootstrap'
-require 'traap.core.clipboard'
-require 'traap.core.customize'
-require 'traap.core.functions'
-require 'traap.core.options'
-require 'traap.core.plugins'
-require 'traap.core.autocmds'
