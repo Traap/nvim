b949b8765626aca34f25a2728d407f54aef5684d
Traap <gary.a.howard@mac.com>
2023-02-10 16:50:41 -0800

    Add nvim-neo-tree.

--- lua/traap/plugins/utilities.lua
+++ lua/traap/plugins/utilities.lua
@@ -287,6 +287,38 @@ return {
   },
 
   -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-neo-tree
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = function()
+      return require('traap.core.customize').neo_tree_nvim
+    end,
+    cmd = "Neotree",
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
+    end,
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+      },
+      window = {
+        position = 'right',
+     },
+    },
+  },
+
+  -------------------------------------------------------------------------- }}}
   -- {{{ nvim-tree
 
   {
