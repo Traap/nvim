68a7930716399112578af273b2db663fefcd139b
Traap <gary.a.howard@mac.com>
2023-02-10 16:49:49 -0800

    Refactor tab and s-tab.

--- lua/traap/plugins/completion.lua
+++ lua/traap/plugins/completion.lua
@@ -4,6 +4,9 @@ return {
 
   {
     'L3MON4D3/LuaSnip',
+    keys = function()
+      return {}
+    end,
   },
 
   -- ----------------------------------------------------------------------- }}}
@@ -37,8 +40,10 @@ return {
       local has_words_before = function()
         -- Deprecated. (Devined in Lua 5.1/LuaJIT, current is Lua 5.4.)
         -- But, the next line does not work without pack statement.
+        unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+        return col ~= 0 and vim.api.nvim_buf_get_lines(
+          0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
       end
 
       -- local check_backspace = function()
@@ -93,14 +98,10 @@ return {
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
-            fallback()
-          -- elseif check_backspace() then
-          --   fallback()
+            cmp.complete()
           else
             fallback()
           end
