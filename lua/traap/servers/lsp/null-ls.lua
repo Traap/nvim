fd125a60d524701a6ab2c3dd76afc49da428e762
Traap <gary.a.howard@mac.com>
2023-02-21 18:14:07 -0800

    Quiet warning information and enable linters.

--- lua/traap/servers/lsp/null-ls.lua
+++ lua/traap/servers/lsp/null-ls.lua
@@ -2,15 +2,21 @@ local  ok, null_ls = pcall(require, 'null-ls')
 if not ok then return end
 
 local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
+
+-- FIXME: Minimize warning information:
+--        This function expects a maximum of 0 argument(s)
+--        but instead it is receiving 1.
+
+local sources = {
+  null_ls.builtins.completion.spell,
+  null_ls.builtins.code_actions.gitsigns,
+  formatting.rubocop,
+  formatting.prettier.with({ extra_args = { "--no-simi", "--single-quote", "--jsx-single-quote" } }),
+  formatting.black.with({ extra_args = { "--fast" } }),
+  formatting.stylua,
+}
+
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.rubocop,
-    formatting.prettier.with({ extra_args = { "--no-simi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.black.with({ extra_args = { "--fast" } }),
-    formatting.stylua,
-  }
+  debug = false, sources = sources,
 })
