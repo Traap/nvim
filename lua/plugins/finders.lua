6535c3681d8762bc4499f016822d291ba0510618
Traap <gary.a.howard@mac.com>
2023-02-26 00:07:22 -0800

    Move neo-tree to explorers.lua.

--- lua/plugins/finders.lua
+++ lua/plugins/finders.lua
@@ -25,41 +25,6 @@ return {
 	},
 
 	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-neo-tree
-
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		enabled = function()
-			return require("config.customize").neo_tree_nvim
-		end,
-		cmd = "Neotree",
-		deactivate = function()
-			vim.cmd([[Neotree close]])
-		end,
-		init = function()
-			vim.g.neo_tree_remove_legacy_commands = 1
-			if vim.fn.argc() == 1 then
-				local stat = vim.loop.fs_stat(vim.fn.argv(0))
-				if stat and stat.type == "directory" then
-					require("neo-tree")
-				end
-			end
-		end,
-		opts = {
-			filesystem = {
-				bind_to_cwd = false,
-				follow_current_file = true,
-			},
-			window = {
-				position = "right",
-			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-
-	-------------------------------------------------------------------------- }}}
 	-- {{{ Telescope
 
 	{
@@ -68,6 +33,7 @@ return {
 			return require("config.customize").telescope
 		end,
 		cmd = "Telescope",
+		keys = false,
 		opts = {
 			defaults = {
 				layout_config = { prompt_position = "top" },
