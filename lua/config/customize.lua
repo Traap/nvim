9b82ee729eb2a4a3a876c2557d3b5162833d6151
Traap <gary.a.howard@mac.com>
2023-02-25 20:39:30 -0800

    Add neomutt terminal.

--- lua/config/customize.lua
+++ lua/config/customize.lua
@@ -92,6 +92,12 @@ Customize.toggleterm = {
 		return t:toggle()
 	end,
 
+	neomutt = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local t = Terminal:new({ cmd = "neomutt", direction = "float" })
+		return t:toggle()
+	end,
+
 	ranger = function()
 		local Terminal = require("toggleterm.terminal").Terminal
 		local t = Terminal:new({ cmd = "ranger", direction = "float" })
