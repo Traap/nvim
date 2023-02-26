6a1772c05c2f34b706f03f8bc391170c39b6b00d
Traap <gary.a.howard@mac.com>
2023-02-26 00:07:00 -0800

    Formatting.

--- lua/plugins/explorers.lua
+++ lua/plugins/explorers.lua
@@ -1,39 +1,76 @@
 return {
-  -- {{{ gem-browse
+	-- {{{ gem-browse
 
-  {
-    "tpope/gem-browse",
-    ft = { "rb" },
-    enabled = function()
-      return require("config.customize").gem_browse
-    end,
-  },
+	{
+		"tpope/gem-browse",
+		ft = { "rb" },
+		enabled = function()
+			return require("config.customize").gem_browse
+		end,
+	},
 
-  -- ----------------------------------------------------------------------- }}}
-  -- {{{ nvim-tree
+	-- ----------------------------------------------------------------------- }}}
+	-- {{{ nvim-neo-tree
 
-  {
-    "kyazdani42/nvim-tree.lua",
-    enabled = function()
-      return require("config.customize").nvim_tree
-    end,
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		enabled = function()
+			return require("config.customize").neo_tree_nvim
+		end,
+		cmd = "Neotree",
+		deactivate = function()
+			vim.cmd([[Neotree close]])
+		end,
+		-- keys = function() return {} end,
+		keys = false,
+		init = function()
+			vim.g.neo_tree_remove_legacy_commands = 1
+			if vim.fn.argc() == 1 then
+				local stat = vim.loop.fs_stat(vim.fn.argv(0))
+				if stat and stat.type == "directory" then
+					require("neo-tree")
+				end
+			end
+		end,
+		opts = {
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = true,
+			},
+			window = {
+				position = "right",
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-    cmd = {
-      "NvimTreeFindFile",
-      "NvimTreeRefresh",
-      "NvimTreeToggle",
-    },
+	-------------------------------------------------------------------------- }}}
+	-- {{{ nvim-tree
 
-    opts = {
-      filters = {
-        custom = { ".git" },
-      },
-      view = {
-        side = "right",
-        width = 30,
-      },
-    },
-  },
+	{
+		"kyazdani42/nvim-tree.lua",
+		enabled = function()
+			return require("config.customize").nvim_tree
+		end,
 
-  -------------------------------------------------------------------------- }}}
+		cmd = {
+			"NvimTreeFindFile",
+			"NvimTreeRefresh",
+			"NvimTreeToggle",
+		},
+
+		opts = {
+			filters = {
+				custom = { ".git" },
+			},
+			view = {
+				side = "right",
+				width = 30,
+			},
+		},
+	},
+
+	-------------------------------------------------------------------------- }}}
 }
