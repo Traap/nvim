eb04379fe5978c38ad292a9e476d089ae037ab09
Traap <gary.a.howard@mac.com>
2023-03-01 21:20:43 -0800

    Remove obsolete lsp_ensure_installed().

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,17 +1,17 @@
-local M  = {}
+local M = {}
 
 function M.keymap(mode, lhs, rhs, opts)
-  local options = {noremap=true, silent=true}
-  options = vim.tbl_deep_extend('force', options, opts or {})
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	options = vim.tbl_deep_extend("force", options, opts or {})
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-M.lsp_ensure_installed = function()
-  local ensure_installed = {}
-  for _, value in pairs(require('config.constants').lsp_to_mason) do
-    table.insert(ensure_installed, value.lsp)
-  end
-  return ensure_installed
-end
+-- M.lsp_ensure_installed = function()
+--   local ensure_installed = {}
+--   for _, value in pairs(require('config.constants').lsp_to_mason) do
+--     table.insert(ensure_installed, value.lsp)
+--   end
+--   return ensure_installed
+-- end
 
 return M
