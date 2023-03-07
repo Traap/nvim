ad0349fd2b6e170b3920a68c0648cc940531ade3
Traap <gary.a.howard@mac.com>
2023-03-07 15:45:37 -0800

    Remove lsp install loop.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,52 +2,40 @@ Constants = require("config.constants")
 Is_Enabled = require("config.functions").is_enabled
 
 return {
-	-- {{{ mason.nvim
+  -- {{{ mason.nvim
 
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		enabled = Is_Enabled("mason.nvim"),
-		keys = { { "<leader>cm", "<cmd>Mason<cr> " } },
-		opts = {
-			ensure_installed = Constants.ensure_installed.mason,
-		},
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    enabled = Is_Enabled("mason.nvim"),
+    keys = { { "<leader>cm", "<cmd>Mason<cr> " } },
+    opts = {
+      ensure_installed = Constants.ensure_installed.mason,
+    },
+  },
 
-		-- config = function(_, opts)
-		-- 	require("mason").setup(opts)
-		-- 	local mr = require("mason-registry")
-		-- 	for _, value in ipairs(opts.ensure_installed) do
-		-- 		local p = mr.get_package(value)
-		-- 		if not p:is_installed() then
-		-- 			p:install()
-		-- 		end
-		-- 	end
-		-- end,
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-lspconfig
 
-	},
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    enabled = Is_Enabled("nvim-lspconfig"),
+    opts = function(_, opts)
+      opts.servers = {
+        jsonls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = { callSnippet = "Replace" },
+              diagnostics = { globals = { "vim" } },
+              workspace = { checkThirdParty = false },
+            },
+          },
+        },
+      }
+    end,
+  },
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-lspconfig
-
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		enabled = Is_Enabled("nvim-lspconfig"),
-		opts = function(_, opts)
-			opts.servers = {
-				jsonls = {},
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = { callSnippet = "Replace" },
-							diagnostics = { globals = { "vim" } },
-							workspace = { checkThirdParty = false },
-						},
-					},
-				},
-			}
-		end,
-	},
-
-	-- ----------------------------------------------------------------------- }}}
+  -- ----------------------------------------------------------------------- }}}
 }
