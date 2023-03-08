a313fbdaea3e67a9379c6cca22be819e41f63074
Traap <gary.a.howard@mac.com>
2023-03-07 22:34:06 -0800

    Say something useful. LOL

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
+-- Set em' first! Things might go sideways while I'm configuring Neovim
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[,]]
 
+-- Use my bootstrap method.  Then hand things over to Lazy.
 require("config.bootstrap")
 require("config.lazy")
 
