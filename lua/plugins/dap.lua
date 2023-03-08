750c5b51896aea94c8c1c7dae8e802456628b8c8
Traap <gary.a.howard@mac.com>
2023-03-07 21:46:58 -0800

    WIP: LSP and DAP changes to support debugging.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,149 +3,147 @@ Constants = require("config.constants")
 Is_Enabled = require("config.functions").is_enabled
 
 return {
-	-- {{{ mason-nvim-dap.nvim
 
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		enabled = Is_Enabled("mason-nvim-dap.nvim"),
-		ft = { "go", "py", "rb" },
-		config = true,
-		-- opts = function(_, opts)
-		-- 	opts.automatic_setup = true
-		-- 	opts.ensure_installed = {
-		-- 		"bash-debug-adapter",
-		-- 		"chrome-debug-adapter",
-		-- 		"codelldb",
-		-- 		"cpptools",
-		-- 		"dart-debug-adapter",
-		-- 		"debugpy",
-		-- 		"delve",
-		-- 		"elixir-ls",
-		-- 		"firefox-debug-adapter",
-		-- 		"java-debug-adapter",
-		-- 		"java-test",
-		-- 		"js-debug-adapter",
-		-- 		"kotlin-debug-adapter",
-		-- 		"mockdebug",
-		-- 		"netcoredbg",
-		-- 		"node-debug2-adapter",
-		-- 		"php-debug-adapter",
-		-- 		"puppet-editor-services",
-		-- 	}
-		-- end,
-	},
+  -- {{{ mason-nvim-dap.nvim
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-dap
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    cmd = "Mason",
+    enabled = true,
+    enabled = Is_Enabled("mason-nvim-dap.nvim"),
+    opts = {
+      automatic_setup = true,
+      ensure_installed = {
+        "codelldb",
+        "debugpy",
+        "delve",
+        "java-debug-adapter",
+        "java-test",
+        "js-debug-adapter",
+        "netconfigdb",
+        "node-debug2-adapter",
+      },
+    },
+  },
 
-	{
-		"mfussenegger/nvim-dap",
-		enabled = Is_Enabled("nvim-dap"),
-		ft = { "go", "py", "rb" },
-		dependencies = {
-			"nvim-telescope/telescope-dap.nvim",
-			"jay-babu/mason-nvim-dap.nvim",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-		},
-	},
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-dap
+  {
+    "mfussenegger/nvim-dap",
+    enabled = Is_Enabled("nvim-dap"),
+    ft = { "c", "cpp", "go", "py", "rb" },
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-dap-ui
+    config = function()
+      require("dapui").setup()
+      require("nvim-dap-virtual-text").setup()
+      -- require("telescope-dap").setup()
+    end,
 
-	{
-		"rcarriga/nvim-dap-ui",
-		enabled = Is_Enabled("nvim-dap-ui"),
-		ft = { "go", "py", "rb" },
-		config = true,
-	},
+    dependencies = {
+      "jbyuki/one-small-step-for-vimkind",
+      "nvim-telescope/telescope-dap.nvim",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+  },
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-dap-virtual-text
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-dap-ui
 
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		enabled = Is_Enabled("nvim-dap-virtual-text"),
-		ft = { "go", "py", "rb" },
-		config = true,
-	},
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   enabled = Is_Enabled("nvim-dap-ui"),
+  --   ft = { "go", "py", "rb" },
+  --   config = true,
+  -- },
 
-	-- ------------------------------------------------------------------------ }}}
-	-- {{{ telescope-dap.nvim
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-dap-virtual-text
 
-	{
-		"nvim-telescope/telescope-dap.nvim",
-		enabled = Is_Enabled("telescope-dap.nvim"),
-		ft = { "go", "py", "rb" },
-		config = true,
-	},
+  -- {
+  --   "theHamsta/nvim-dap-virtual-text",
+  --   enabled = Is_Enabled("nvim-dap-virtual-text"),
+  --   ft = { "go", "py", "rb" },
+  --   config = true,
+  -- },
 
-	-- ------------------------------------------------------------------------ }}}
-	-- {{{ crates.nvim
+  -- ------------------------------------------------------------------------ }}}
+  -- {{{ telescope-dap.nvim
 
-	{
-		"saecki/crates.nvim",
-		enabled = Is_Enabled("crates.nvim"),
-		dependencies = { "mfussenegger/nvim-dap" },
-		event = "VeryLazy",
-		ft = { "rs" },
-		opts = {
-			null_ls = {
-				name = "crates.nvim",
-			},
-			popup = {
-				border = "rounded",
-			},
-		},
-	},
+  -- {
+  -- "nvim-telescope/telescope-dap.nvim",
+  -- enabled = Is_Enabled("telescope-dap.nvim"),
+  -- ft = { "go", "py", "rb" },
+  -- config = true,
+  -- },
 
-	-- ------------------------------------------------------------------------ }}}
-	-- {{{ nvim-dap-go
+  -- ------------------------------------------------------------------------ }}}
+  -- {{{ crates.nvim
 
-	{
-		"leoluz/nvim-dap-go",
-		enabled = Is_Enabled("nvim-dap-go"),
-		dependencies = { "mfussenegger/nvim-dap" },
-		event = "VeryLazy",
-		ft = { "go" },
-		config = true,
-		-- opts = {
-		-- 	dap_configurations = {
-		-- 		{
-		-- 			type = "go",
-		-- 			name = "Attach remote",
-		-- 			mode = "remote",
-		-- 			request = "attach",
-		-- 		},
-		-- 	},
-		-- 	delve = {
-		-- 		initialize_timeout_sec = 20,
-		-- 		port = "${port}",
-		-- 	},
-		-- },
-	},
+  -- {
+  --   "saecki/crates.nvim",
+  --   enabled = Is_Enabled("crates.nvim"),
+  --   dependencies = { "mfussenegger/nvim-dap" },
+  --   event = "VeryLazy",
+  --   ft = { "rs" },
+  --   opts = {
+  --     null_ls = {
+  --       name = "crates.nvim",
+  --     },
+  --     popup = {
+  --       border = "rounded",
+  --     },
+  --   },
+  -- },
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-dap-python
-	{
-		"mfussenegger/nvim-dap-python",
-		enabled = Is_Enabled("nvim-dap-python"),
-		dependencies = { "mfussenegger/nvim-dap" },
-		event = "VeryLazy",
-		ft = { "py" },
-		-- config = true,
-	},
+  -- ------------------------------------------------------------------------ }}}
+  -- {{{ nvim-dap-go
 
-	-- ----------------------------------------------------------------------- }}}
-	-- {{{ nvim-dap-ruby
-	{
-		"suketa/nvim-dap-ruby",
-		enabled = Is_Enabled("nvim-dap-ruby"),
-		dependencies = { "mfussenegger/nvim-dap" },
-		event = "VeryLazy",
-		ft = { "rb" },
-		config = true,
-	},
+  {
+    "leoluz/nvim-dap-go",
+    enabled = Is_Enabled("nvim-dap-go"),
+    dependencies = { "mfussenegger/nvim-dap" },
+    event = "VeryLazy",
+    ft = { "go" },
+    config = true,
+    opts = {
+      dap_configurations = {
+        {
+          type = "go",
+          name = "Attach remote",
+          mode = "remote",
+          request = "attach",
+        },
+      },
+      delve = {
+        initialize_timeout_sec = 20,
+        port = "${port}",
+      },
+    },
+  },
 
-	-- ----------------------------------------------------------------------- }}}
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-dap-python
+
+  {
+    "mfussenegger/nvim-dap-python",
+    enabled = Is_Enabled("nvim-dap-python"),
+    dependencies = { "mfussenegger/nvim-dap" },
+    event = "VeryLazy",
+    ft = { "py" },
+  },
+
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ nvim-dap-ruby
+
+  {
+    "suketa/nvim-dap-ruby",
+    enabled = Is_Enabled("nvim-dap-ruby"),
+    dependencies = { "mfussenegger/nvim-dap" },
+    event = "VeryLazy",
+    ft = { "rb" },
+    config = true,
+  },
+
+  -- ----------------------------------------------------------------------- }}}
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,13 +23,81 @@ return {
     enabled = Is_Enabled("nvim-lspconfig"),
     opts = function(_, opts)
       opts.servers = {
+        ansiblels = {},
+        astro = {},
+        bashls = {},
+        clangd = {},
+        cssls = {},
+        denols = false,
+        dockerls = {},
+        gopls = {},
+        html = {},
         jsonls = {},
+        marksman = {},
+        pyright = {},
+        svelte = {},
+        tsserver = {},
+        yamlls = {},
+
         lua_ls = {
+          single_file_support = true,
           settings = {
             Lua = {
-              completion = { callSnippet = "Replace" },
-              diagnostics = { globals = { "vim" } },
-              workspace = { checkThirdParty = false },
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                workspaceWord = true,
+                callSnippet = "Both",
+              },
+              misc = {
+                parameters = {
+                  "--log-level=trace",
+                },
+              },
+              diagnostics = {
+                -- enable = false,
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
+              format = {
+                enable = false,
+                defaultConfig = {
+                  indent_style = "space",
+                  indent_size = "2",
+                  continuation_indent_size = "2",
+                },
+              },
+            },
+          },
+        },
+
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              procMacro = { enable = true },
+              cargo = { allFeatures = true },
+              checkOnSave = {
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
             },
           },
         },
