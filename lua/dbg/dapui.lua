72a5099d05f9ec449b257253875c76e022cb3b2e
Traap <gary.a.howard@mac.com>
2023-03-07 21:42:47 -0800

    Stylua formatting.

--- lua/dbg/dapui.lua
+++ lua/dbg/dapui.lua
@@ -3,13 +3,13 @@
 local dap, dapui = require("dap"), require("dapui")
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-	dapui.open()
+  dapui.open()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
+  dapui.close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
+  dapui.close()
 end
