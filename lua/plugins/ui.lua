dc043b5ad47e248d384758aedced390b2dd5accf
Traap <gary.a.howard@mac.com>
2023-03-08 11:30:13 -0800

    2nd try: Customzie lualine and Youtube video prep.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,63 +11,6 @@ return {
   },
 
   -- ----------------------------------------------------------------------- }}}
-  -- {{{ lualine.nvim
-
-  {
-    "nvim-lualine/lualine.nvim",
-    enabled = Is_Enabled("lualine.nvim"),
-    opts = function(_, opts)
-      -- Youtube: 2nd true -> false
-      if true == false then
-        opts.options = {
-          icons_enabled = true,
-          theme = "auto",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          },
-        }
-
-        opts.sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        }
-
-        opts.inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        }
-
-        opts.tabline = {}
-        opts.winbar = {}
-        opts.inactive_winbar = {}
-        opts.extensions = {}
-      else
-        -- Use default LazyVim setup.
-        opts = {}
-      end
-    end,
-  },
-
-  -- ----------------------------------------------------------------------- }}}
   -- {{{ dressing
 
   {
@@ -158,6 +101,63 @@ return {
       show_trailing_blankline_indent = false,
       show_current_context = false,
     },
+  },
+
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ lualine.nvim
+
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = Is_Enabled("lualine.nvim"),
+    opts = function(_, opts)
+      -- Youtube: 2nd true -> false
+      if true == true then
+        opts.options = {
+          icons_enabled = true,
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          },
+        }
+
+        opts.sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        }
+
+        opts.inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        }
+
+        opts.tabline = {}
+        opts.winbar = {}
+        opts.inactive_winbar = {}
+        opts.extensions = {}
+      else
+        -- Use default LazyVim setup.
+        opts = {}
+      end
+    end,
   },
 
   -- ----------------------------------------------------------------------- }}}
