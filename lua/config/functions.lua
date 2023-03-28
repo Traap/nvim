129dfa1c87290ed39e64df48ce86eb5f7dab1950
Traap <gary.a.howard@mac.com>
2023-03-28 16:31:32 -0700

    Add Youtube comments for next video.

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,5 +1,4 @@
 Customzie = require("config.customize")
-
 local M = {}
 
 function M.keymap(mode, lhs, rhs, opts)
@@ -8,20 +7,24 @@ function M.keymap(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-function M.use_plugin_defaults(plugin)
-  return Customize.plugins[plugin].defaults or false
-end
-
+-- Youtube: Is this plugin enabled?
 function M.is_enabled(plugin)
   return Customize.plugins[plugin].enabled
 end
 
+-- Youtube: Are we ready to debug?
 function M.is_debugger(debugger)
   return Customize.debuggers[debugger].enabled
 end
 
+-- Youtube: Are we inside TMUX?
 function M.in_tmux()
   return os.getenv("TMUX") ~= nil
+end
+
+-- Youtube: Use LazyVim defaults
+function M.use_plugin_defaults(plugin)
+  return Customize.plugins[plugin].defaults or false
 end
 
 return M
