f95df0726a0cf3736f921f3a170b35f57196c814
Traap <gary.a.howard@mac.com>
2023-03-28 17:37:49 -0700

    Prototype surround selected text.
    The function does not work.  Check code TODO notes.

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -27,4 +27,26 @@ function M.use_plugin_defaults(plugin)
   return Customize.plugins[plugin].defaults or false
 end
 
+-- TODO: This prototype is broken.
+function M.surround_selected_text()
+  -- Get current buffer.
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Get the start and end positions of the visual selection
+  local sline = vim.api.nvim_buf_get_mark(bufnr, "<")
+  local eline = vim.api.nvim_buf_get_mark(bufnr, ">")
+
+  -- NOTE: 1) Relativenumbers cause grief. 2) Marks seam to move around.  Values
+  -- reported trough etxt and stxt don't make sense.
+
+  local start = sline[1] - 1
+  local finish = eline[1] + 1
+
+  local etxt = { "-- finish: " .. finish }
+  vim.api.nvim_buf_set_lines(bufnr, finish, finish, false, etxt)
+
+  local stxt = { "-- start: " .. start }
+  vim.api.nvim_buf_set_lines(bufnr, start, start, false, stxt)
+end
+
 return M
