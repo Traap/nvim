c6a555110cea11cdc61a9b3d3e9140872f7ccc1b
Traap <gary.a.howard@mac.com>
2023-04-05 09:30:11 -0700

    Add comments around new functions.

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,31 +1,35 @@
 Customzie = require("config.customize")
 local M = {}
 
+-- {{{ keymap helper
+
 function M.keymap(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   options = vim.tbl_deep_extend("force", options, opts or {})
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Youtube: Is this plugin enabled?
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Boolean helpers
+
 function M.is_enabled(plugin)
   return Customize.plugins[plugin].enabled
 end
 
--- Youtube: Are we ready to debug?
 function M.is_debugger(debugger)
   return Customize.debuggers[debugger].enabled
 end
 
--- Youtube: Are we inside TMUX?
 function M.in_tmux()
   return os.getenv("TMUX") ~= nil
 end
 
--- Youtube: Use LazyVim defaults?
 function M.use_plugin_defaults(plugin)
   return Customize.plugins[plugin].defaults or false
 end
+
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Surround selected text
 
 -- TODO: This prototype is broken.
 function M.surround_selected_text()
@@ -48,5 +52,7 @@ function M.surround_selected_text()
   local stxt = { "-- start: " .. start }
   vim.api.nvim_buf_set_lines(bufnr, start, start, false, stxt)
 end
+
+-- ------------------------------------------------------------------------- }}}
 
 return M
