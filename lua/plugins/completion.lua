4739eefddc37461a089cb456ca89faf979faf381
Traap <gary.a.howard@mac.com>
2023-04-10 19:17:18 -0700

    Borrow from AstroNvim for completion.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,9 +3,6 @@ Customize = require("config.customize")
 Is_Enabled = require("config.functions").is_enabled
 
 return {
-  -- {{{ nvim-cmp
-  --     TODO: Try to improve nested folds behavior.
-
   {
     -- {{{ Events and dependencies
 
@@ -26,24 +23,11 @@ return {
     },
 
     -- --------------------------------------------------------------------- }}}
-    -- {{{ opts function overfides LazyVim default behavior.
+    -- {{{ opts function begins and overrides LazyVim default behavior.
 
     opts = function(_, opts)
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-
-    -- --------------------------------------------------------------------- }}}
-      -- {{{ Formatting fileds, icons, and source_mapping
-
-      local formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          vim_item.kind =
-            string.format("%s", Constants.icons.lsp_kinds[vim_item.kind])
-          vim_item.menu = (Constants.completion.source_mapping)[entry.source.name]
-          return vim_item
-        end,
-      }
 
       -- ------------------------------------------------------------------- }}}
       -- {{{ Confirmaiton option
@@ -87,16 +71,81 @@ return {
       })
 
       -- ------------------------------------------------------------------- }}}
+      -- {{{ Has words before
+
+      local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      -- ------------------------------------------------------------------- }}}
+      -- {{{ Snippets
+
+      local snippet = {
+        expand = function(args) luasnip.lsp_expand(args.body) end,
+      }
+
+      -- ------------------------------------------------------------------- }}}
+      -- {{{ Mappings
+
+      local mapping = {
+        ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+
+        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+
+        ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
+
+        ["<C-y>"] = cmp.config.disable,
+
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }
+
+      -- ------------------------------------------------------------------- }}}
       -- {{{ Update the function argument opts with local choices made.
 
       opts.confirm_opts = confirm_opts
-      opts.formatting = formatting
+      opts.mapping = mapping
+      opts.snippet = snippet
       opts.sources = Constants.completion.sources
       opts.window = window
 
       -- ------------------------------------------------------------------- }}}
-    end,
-  },
+      -- {{{ opts function ends
 
-  -- ----------------------------------------------------------------------- }}}
+    end,
+
+    -- --------------------------------------------------------------------- }}}
+  },
 }
