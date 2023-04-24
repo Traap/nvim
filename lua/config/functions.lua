370ee8fca94213b6e72e8dfd37205a95b29bbe81
Traap <gary.a.howard@mac.com>
2023-04-24 14:03:17 -0700

    Remove Windoz 32 cmd.exe

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -32,26 +32,26 @@ end
 -- {{{ Surround selected text
 
 -- TODO: This prototype is broken.
-function M.surround_selected_text()
-  -- Get current buffer.
-  local bufnr = vim.api.nvim_get_current_buf()
+-- function M.surround_selected_text()
+--   -- Get current buffer.
+--   local bufnr = vim.api.nvim_get_current_buf()
 
-  -- Get the start and end positions of the visual selection
-  local sline = vim.api.nvim_buf_get_mark(bufnr, "<")
-  local eline = vim.api.nvim_buf_get_mark(bufnr, ">")
+--   -- Get the start and end positions of the visual selection
+--   local sline = vim.api.nvim_buf_get_mark(bufnr, "<")
+--   local eline = vim.api.nvim_buf_get_mark(bufnr, ">")
 
-  -- NOTE: 1) Relativenumbers cause grief. 2) Marks seam to move around.  Values
-  -- reported trough etxt and stxt don't make sense.
+--   -- NOTE: 1) Relativenumbers cause grief. 2) Marks seam to move around.  Values
+--   -- reported trough etxt and stxt don't make sense.
 
-  local start = sline[1] - 1
-  local finish = eline[1] + 1
+--   local start = sline[1] - 1
+--   local finish = eline[1] + 1
 
-  local etxt = { "-- finish: " .. finish }
-  vim.api.nvim_buf_set_lines(bufnr, finish, finish, false, etxt)
+--   local etxt = { "-- finish: " .. finish }
+--   vim.api.nvim_buf_set_lines(bufnr, finish, finish, false, etxt)
 
-  local stxt = { "-- start: " .. start }
-  vim.api.nvim_buf_set_lines(bufnr, start, start, false, stxt)
-end
+--   local stxt = { "-- start: " .. start }
+--   vim.api.nvim_buf_set_lines(bufnr, start, start, false, stxt)
+-- end
 
 -- ------------------------------------------------------------------------- }}}
 -- {{{ User event
@@ -76,9 +76,7 @@ end
 ---@param show_error? boolean Whether or not to show an unsuccessful command as an error to the user
 ---@return string|nil # The result of a successfully executed command or nil
 function M.cmd(cmd, show_error)
-  local wind32_cmd
-  if vim.fn.has "win32" == 1 then wind32_cmd = { "cmd.exe", "/C", cmd } end
-  local result = vim.fn.system(wind32_cmd or cmd)
+  local result = cmd
   local success = vim.api.nvim_get_vvar "shell_error" == 0
   if not success and (show_error == nil or show_error) then
     vim.api.nvim_err_writeln("Error running command: " .. cmd .. "\nError message:\n" .. result)
