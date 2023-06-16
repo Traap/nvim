d59ad24387243d136ae9615297427b03f3f0cb8f
Traap <gary.a.howard@mac.com>
2023-06-16 14:30:31 -0700

    Enable completion on UIEnter event.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,19 +8,22 @@ local plugin = "nvim-cmp"
 
 return {
   "hrsh7th/" .. plugin,
-  event = "InsertEnter",
+  event = {"UIEnter"},
   enabled = Is_Enabled(plugin),
   opts = require("plugins.opts." .. plugin),
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-calc",
     "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+    "f3fora/cmp-spell",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-nvim-lsp-document-symbol",
     "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
-    "f3fora/cmp-spell",
     "saadparwaiz1/cmp_luasnip",
   }
 }
+
+
+-- This
 -- ------------------------------------------------------------------------- }}}
