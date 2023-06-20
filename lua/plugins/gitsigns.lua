ce3b48eadf67c7789e7f460a25c0c8b70a7f81a5
Traap <gary.a.howard@mac.com>
2023-06-19 20:10:37 -0700

    Customize gitsigns.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,8 +8,9 @@ local plugin = "gitsigns.nvim"
 return {
   "lewis6991/" .. plugin,
   event = { "BufReadPost", "BufNewFile" },
-  enabled = Is_Enabled(plugin) and vim.fn.executable == 1,
-  ft = "gitcommit",
+  enabled = Is_Enabled(plugin),
+  -- ft = "gitcommit",
+
   opts = function(_, opts)
     if Use_Defaults(plugin) then
       opts = opts
@@ -17,33 +18,18 @@ return {
       opts.signs = {
         add = { text = "▎" },
         change = { text = "▎" },
-        delete = { text = "契" },
-        topdelete = { text = "契" },
+        delete = { text = "契"},
+        topdelete = { text = "契"},
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       }
-
-      opts.on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-        end
-
-        -- stylua: ignore start
-        map('n', ']h', gs.next_hunk, 'Next Hunk')
-        map('n', '[h', gs.prev_hunk, 'Prev Hunk')
-        map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
-        map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
-        map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
-        map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
-        map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
-        map('n', '<leader>ghp', gs.preview_hunk, 'Preview Hunk')
-        map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
-        map('n', '<leader>ghd', gs.diffthis, 'Diff This')
-        map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
-      end
+      opts.current_line_blame = true
+      opts.current_line_blame_formatter = " <author>, <author_time:%Y-%m-%d> - <summary>"
+      opts.trouble = false
     end
+  end,
+
+  config = function(_, opts)
+    require('gitsigns').setup(opts)
   end,
 }
