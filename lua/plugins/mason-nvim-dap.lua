e3406508381115f863af18ca13aec98fcc06b0e5
Traap <gary.a.howard@mac.com>
2023-07-02 16:55:02 -0700

    Steamline mason-nvim-dap configuration.

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -1,6 +1,6 @@
-local functions = require("config.functions")
-Is_Enabled = functions.is_enabled
-Use_Defaults = functions.use_plugin_defaults
+Customize = require("config.customize")
+Is_Enabled = require("config.functions").is_enabled
+Use_Defaults = require("config.functions").use_plugin_defaults
 
 local plugin = "mason-nvim-dap.nvim"
 
@@ -9,11 +9,20 @@ return {
   enabled = Is_Enabled(plugin),
   event = Constants.file_types,
   opts = function(_, opts)
-      opts = opts
     if Use_Defaults(plugin) then
+      opts = opts
     else
       opts.automatic_setup = true
-      opts.ensure_installed = Constants.ensure_installed.dap
+      opts.ensure_installed = {
+        "codelldb",
+        "debugpy",
+        "delve",
+        "java-debug-adapter",
+        "java-test",
+        "js-debug-adapter",
+        "netconfigdb",
+        "node-debug2-adapter",
+      }
     end
   end,
 }
