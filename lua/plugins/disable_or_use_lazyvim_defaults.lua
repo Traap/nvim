b806c22d63f7869cd9533b28f9daa2a51884997a
Traap <gary.a.howard@mac.com>
2023-07-02 22:07:23 -0700

    Add mini.pairs plugin configuration.

--- lua/plugins/disable_or_use_lazyvim_defaults.lua
+++ lua/plugins/disable_or_use_lazyvim_defaults.lua
@@ -9,7 +9,6 @@ require("config.globals")
 
 return {
   { "catppuccin/nvim", enabled = Is_Enabled("catppuccin"), },
-  { "echasnovski/mini.pairs", enabled = Is_Enabled("mini.pairs") },
   { "echasnovski/mini.surround", enabled = Is_Enabled("mini.surround") },
   { "folke/neoconf.nvim", enabled = Is_Enabled("neoconf.nvim") },
   { "folke/neodev.nvim", enabled = Is_Enabled("neodev.nvim") },
