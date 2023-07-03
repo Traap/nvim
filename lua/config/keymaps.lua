63dad530164420ec8aa61d14dc010f49f18bc2f8
Traap <gary.a.howard@mac.com>
2023-07-03 15:02:06 -0700

    Move dispatch keymaps to plugin configuration.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -64,25 +64,6 @@ Keymap("n", "<localleader>E", [[0mMvg_"ky :exec "r!" getreg("k")<cr>]])
 Keymap("v", "<localleader>E", [["ky :exec "r!" getreg("k")<cr>]])
 
 -- ------------------------------------------------------------------------- }}}
--- {{{ TMUX Sessions.
-
-Keymap("n", "<leader>tao",   "<cmd>Dispatch!ao ao<cr>")
-Keymap("n", "<leader>tbash", "<cmd>Dispatch!ao bash<cr>")
-Keymap("n", "<leader>tkjv",  "<cmd>Dispatch!ao kjv<cr>")
-Keymap("n", "<leader>tssh",  "<cmd>Dispatch!ao ssh<cr>")
-Keymap("n", "<leader>tsoup", "<cmd>Dispatch!ao soup<cr>")
-Keymap("n", "<leader>tvim",  "<cmd>Dispatch!ao vim<cr>")
-Keymap("n", "<leader>twiki", "<cmd>Dispatch!ao wiki<cr>")
-
-Keymap("n", "<leader>kao",   "<cmd>Dispatch! tmux kill-session -t ao<cr>")
-Keymap("n", "<leader>kbash", "<cmd>Dispatch! tmux kill-session -t bash<cr>")
-Keymap("n", "<leader>kkjv",  "<cmd>Dispatch! tmux kill-session -t kjv<cr>")
-Keymap("n", "<leader>kssh",  "<cmd>Dispatch! tmux kill-session -t ssh<cr>")
-Keymap("n", "<leader>ksoup", "<cmd>Dispatch! tmux kill-session -t soup<cr>")
-Keymap("n", "<leader>kvim",  "<cmd>Dispatch! tmux kill-session -t vim<cr>")
-Keymap("n", "<leader>kwiki", "<cmd>Dispatch! tmux kill-session -t wiki<cr>")
-
--- ------------------------------------------------------------------------- }}}
 -- {{{ Q/q - Quit
 
 -- Quit all
