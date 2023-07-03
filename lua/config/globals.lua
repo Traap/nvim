5b97c3744bc686b804ffa34ba679a08ccfd5eeef
Traap <gary.a.howard@mac.com>
2023-07-02 18:41:31 -0700

    Update globals documentation.

---
+++ lua/config/globals.lua
@@ -0,0 +1,7 @@
+-- My plugin customizations reuire the following global values.
+
+Constants = require("config.constants")
+Customize = require("config.customize")
+Is_Enabled = require("config.functions").is_enabled
+Use_Defaults = require("config.functions").use_plugin_defaults
+In_Tmux = require("config.functions").in_tmux
