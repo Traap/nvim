d0f09e199dfa5ab60b083eaf8ff1cd06ac116c37
Traap <gary.a.howard@mac.com>
2023-07-02 22:46:05 -0700

    Update mason-nvim-dap plugin configuration.

--- lua/plugins/disable_or_use_lazyvim_defaults.lua
+++ lua/plugins/disable_or_use_lazyvim_defaults.lua
@@ -9,7 +9,6 @@ require("config.globals")
 
 return {
   { "catppuccin/nvim", enabled = Is_Enabled("catppuccin"), },
-  { "jay-babu/mason-nvim-dap.nvim", enabled = Is_Enabled("mason-nvim-dap.nvim") },
   { "jose-elias-alvarez/null-ls.nvim", enabled = Is_Enabled("null-ls.nvim") },
   { "mfussenegger/nvim-dap", enabled = Is_Enabled("nvim-dap") },
   { "stevearc/dressing.nvim", enabled = Is_Enabled("dressing") },
