d43707eb843166597802ee3e9ae341103d9ef74d
Traap <gary.a.howard@mac.com>
2023-07-12 22:36:10 -0700

    Redisplay a WikiExport pdf document.

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -44,27 +44,45 @@ function M.use_default_keys(plugin)
   return Customize.plugins[plugin].default.keys or false
 end
 
--- FIX: Parse pagename correctly.
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Wiki helper
+
+-- Use PDF_VIEWER to view pdf document of prior WikiExport.  WikiExports are
+-- done to WIKIHOME/printed.
+--
+-- TODO: Notify user when PDF_VIEWER does not exits.
+-- TODO: Notify user when WIKIHOME does not exits.
+-- TODO: Notify user when WikiExport document does not exist.
 function M.page_viewer()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local pagename = vim.api.nvim_buf_get_name(bufnr)
-  local command = "<cmd>!"..PDF_VIEWER.." "..os.getenv("WIKIHOME").."/printed/"..pagename..".pdf<cr>"
-  M.log_error(command)
-  return command
+  local pdf_viewer = os.getenv("PDF_VIEWER")
+  local wiki_home = os.getenv("WIKIHOME")
+  local page_name = vim.fn.fnameescape(vim.fn.expand('%:p:t:r'))
+
+  local cmd_string = pdf_viewer.." "..wiki_home.."/printed/"..page_name..".pdf"
+
+  local command = {
+    cmd = 'Dispatch',
+    bang = true,
+    args = { cmd_string }
+  }
+
+  vim.api.nvim_cmd(command, {})
+
 end
 
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Error logger
 
 function M.log_error(errorMessage)
-  local log_file = io.open("/home/traap/neovim.log", 'a')
-
+  local log_file = io.open(os.getenv("HOME").."/neovim.log", 'a')
   if log_file then
     log_file:write(errorMessage .. "\n")
     log_file:close()
   else
     print("Error opening log file!")
   end
-
 end
+
 -- ------------------------------------------------------------------------- }}}
 
 return M
--- lua/plugins/wiki-vim.lua
+++ lua/plugins/wiki-vim.lua
@@ -1,17 +1,6 @@
+
 require("config.globals")
-
 local plugin = "wiki.vim"
-
-if Is_Enabled(plugin) then
-  if vim.fn.empty(os.getenv("WSL_DISTRO_NAME")) ~= 0 then
-    PDF_VIEWER = "okular"
-  else
-    PDF_VIEWER = "SumatraPDF.exe"
-  end
-
-  Keymap("n", "<leader>wv", '<cmd>lua Page_Viewer()<cr>')
-
-end
 
 return {
   "lervag/" .. plugin,
@@ -24,11 +13,12 @@ return {
     "Traap/vim-bundle-wiki.vim",
   },
   keys = {
-    {"<leader>wa", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Acronyms.md<cr>", "Wiki Acronyms"},
     {"<leader>wb", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Backlog.md<cr>", "Wiki Backlog"},
+    {"<leader>wa", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Acronyms.md<cr>", "Wiki Acronyms"},
     {"<leader>we", "<cmd>WikiExport<cr>", "Wiki Export"},
     {"<leader>wi", "<cmd>WikiIndex<cr>", "Wiki Index"},
     {"<leader>wj", "<cmd>WikiJournal<cr>", "Wiki Journal"},
     {"<leader>wp", "<cmd>WikiFzfPages<cr>", "Wiki FzF Pages"},
+    {"<leader>wv", "<cmd> lua Page_Viewer()<cr>"}
   }
 }
