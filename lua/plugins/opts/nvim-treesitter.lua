68aa745aff93712bf40899d6097a8071b9acb176
Traap <gary.a.howard@mac.com>
2023-07-21 22:38:52 -0700

    Refactor nvim-treesitter options.

--- lua/plugins/opts/nvim-treesitter.lua
+++ lua/plugins/opts/nvim-treesitter.lua
@@ -1,27 +1,2 @@
 return function(_, opts)
-  opts.autopairs = { enable = true }
-  opts.autotag = { enable = true, disable = { "xml" } }
-  opts.context_commenting = { enable = true, enable_autocmd = false }
-  opts.highlight = {
-    enable = true,
-    disabled = {
-      "css", "latex", "markdown",
-    },
-    additional_vim_regex_highlighting = true,
-  }
-  opts.indent = { enable = true, disable = { "yml", "yaml" } }
-  opts.rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = 1500,
-    colors = {
-      "Gold", "Orchid", "DodgerBlue", "Cornsilk", "Salmon", "LawnGreen",
-    },
-  }
-  opts.disable = { "latex" }
-  opts.ensure_installed = {
-    "bash", "c", "c_sharp", "cpp", "dockerfile", "html", "javascript", "json",
-    "lua", "markdown", "markdown_inline", "python", "query", "regex", "ruby",
-    "rust", "sql", "toml", "tsx", "typescript", "vim", "vimdoc", "yaml",
-  }
 end
