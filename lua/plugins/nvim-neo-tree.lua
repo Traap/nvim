57bbf50962b40b4499a1b5479b48f2a2413912af
Traap <gary.a.howard@mac.com>
2023-08-04 11:50:22 -0700

    Upgrade customizations.
    1. Remove flit and leap.  LazyVim flash is now the default.
    2. neo-tree updates.
      a. Adjust conditional used to set keybindings.
      b. Adjust to filesystem.follow_current_file.enable.

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -3,7 +3,9 @@ require("config.traap.globals")
 local plugin = "neo-tree.nvim"
 
 -- Note:  nvim-tree takes precedence when both neo-tree.nvim and nvim-tree
-if Is_Enabled(plugin) and not Is_Enabled("nvim-tree") then
+if Is_Enabled(plugin) and
+  not Use_Default_Keys(plugin) and
+  not Is_Enabled("nvim-tree") then
   Keymap("n", "<c-n>", "<cmd>Neotree toggle<cr>")
   Keymap("n", "<leader>nf", "<cmd>Neotree focus<cr>")
   Keymap("n", "<leader>nr", "<cmd>Neotree show<cr>")
@@ -17,7 +19,10 @@ return {
     if Use_Default_Opts(plugin) then
       opts = opts
     else
-      opts.filesystem = { bind_to_cwd = false, follow_current_file = true, }
+      opts.filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true}
+      }
       opts.window = { position = "right", }
     end
   end,
