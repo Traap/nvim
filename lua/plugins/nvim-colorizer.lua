947b8655a80ea055cbeb2762e64c3eb761f62028
Traap <gary.a.howard@mac.com>
2023-09-04 11:03:04 -0700

    Enable and config NvChad.nvim-colorizer.lua

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -2,8 +2,15 @@ require("config.traap.globals")
 
 local plugin = "nvim-colorizer.lua"
 
+-- Colorizer is enabled and or config through settings in Customize.lua
+-- Red green and blue are colorized when plugin is enabled and config true.
+-- Red  = #FF0000
+-- Green = #00FF00
+-- Blue = #0000FF
+--
 return {
   "NvChad/" .. plugin,
-  event = { "BufReadPost", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" },
   enabled = Is_Enabled(plugin),
+  config = Use_Default_Config(plugin)
 }
