5ef878cddd3b70eaf4362561fb13ec517ca0cfd8
Traap <gary.a.howard@mac.com>
2023-10-14 22:48:50 -0700

    Add c-j and c-k to Telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -59,9 +59,16 @@ return {
     if Use_Default_Opts(plugin) then
       opts = opts
     else
+      local actions = require("telescope.actions")
       opts.defaults = {
         layout_config = { prompt_position = "top" },
         layout_strategy = "horizontal",
+        mappings = {
+          i = {
+            ["<c-j>"] = actions.move_selection_next,
+            ["<c-k>"] = actions.move_selection_previous,
+          },
+        },
         prompt_prefix = " ",
         selection_caret = " ",
         sorting_strategy = "ascending",
