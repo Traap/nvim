f3f68c29afa91110f08f2012b46034f8d881b2fa
Traap <gary.a.howard@mac.com>
2023-10-22 09:12:51 -0700

    Resolve LazyVim conflicts with vim-tmux-navigator.

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -4,6 +4,6 @@ local plugin = "vim-tmux-navigator"
 
 return {
   "christoomey/" .. plugin,
-  enabled = Is_Enabled(plugin) and In_Tmux(),
+  enabled = Is_Enabled(plugin),
   event =  "VeryLazy",
 }
