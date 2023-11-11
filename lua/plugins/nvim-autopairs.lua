9c0940bdd196ba83c8702aa088acc09e4807b07a
Traap <gary.a.howard@mac.com>
2023-11-11 00:46:05 -0800

    Customize autopairs.

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -6,5 +6,19 @@ return {
   "windwp/" .. plugin,
   event = { "BufReadPost", "BufNewFile" },
   enabled = Is_Enabled(plugin),
-  config = true,
+  opts = {
+    check_ts = true,
+    ts_config = { java = false },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0,
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+  },
 }
