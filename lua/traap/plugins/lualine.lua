c83ed60a5d1e792f53a2b3265806dd38ace89078
Traap <gary.a.howard@mac.com>
2023-11-21 20:36:56 -0800

    chore: change lualine event.

--- lua/traap/plugins/lualine.lua
+++ lua/traap/plugins/lualine.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
-  -- event = 'VeryLazy' ,
-  event = {"BufReadPost", "BufNewFile"},
-  enabled = true,
+  event = 'VeryLazy',
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
