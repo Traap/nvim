11cfab6a879a9a6dba9c9dd3461335faa865da55
Traap <gary.a.howard@mac.com>
2023-11-23 21:18:16 -0800

    Apply noice.nvim macro detection to lualine_x

--- lua/traap/plugins/lualine.lua
+++ lua/traap/plugins/lualine.lua
@@ -6,41 +6,9 @@ return {
   },
 
   opts = function(_, opts)
-    local function show_macro_recording()
-      local recording_register = vim.fn.reg_recording()
-      if recording_register == "" then
-        return ""
-      else
-        return "Recording @" .. recording_register
-      end
-    end
-
-    vim.api.nvim_create_autocmd("RecordingEnter", {
-      callback = function()
-        require("lualine").refresh({
-          place = {"statusline"},
-        })
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("RecordingLeave", {
-      callback = function()
-        local timer = vim.loop.new_timer()
-        timer:start(
-        50,
-        0,
-        vim.schedule_wrap(function()
-          require("lualine").refresh({
-            place = {"statusline"},
-          })
-        end)
-        )
-      end,
-    })
-
     opts.options = {
       icons_enabled = true,
-      theme = "tokyonight",
+      theme = "tokyonight-night",
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
@@ -62,16 +30,28 @@ return {
       lualine_b = { "branch", "diff", "diagnostics" },
       lualine_c = { "filename" },
 
-      lualine_x = { show_macro_recording, "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+
+      lualine_x = {
+        { -- Recording macro
+          require("noice").api.status.mode.get,
+          cond = require("noice").api.status.mode.has,
+          color = { fg = "#ff9e64" },
+        },
+
+        {
+          "progress"
+        },
+      },
+      lualine_y = { "fileformat", "filetype" },
+      lualine_z = { "encoding" },
     }
 
     opts.inactive_sections = {
       lualine_a = {},
       lualine_b = {},
       lualine_c = { "filename" },
-      lualine_x = { "location" },
+
+      lualine_x = { "fileformat", "filetype" },
       lualine_y = {},
       lualine_z = {},
     }
