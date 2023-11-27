0150bb4707408cea2635ad9286d15aae20ce9216
Traap <gary.a.howard@mac.com>
2023-11-27 10:50:42 -0800

    Personalize Mason look and feel.

--- lua/traap/plugins/mason.lua
+++ lua/traap/plugins/mason.lua
@@ -1,1 +1,20 @@
-return { "williamboman/mason.nvim" }
+return {
+  "williamboman/mason.nvim",
+  enable = true,
+  keys = {
+    { "<leader>pm", "<cmd>Mason<cr>", "Mason toggle" },
+  },
+
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        },
+        border = "rounded",
+      },
+    })
+  end,
+}
