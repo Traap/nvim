29d55e8f2ddbda9577b13e94c5c51422e9e2f70c
Traap <gary.a.howard@mac.com>
2023-11-30 22:51:01 -0800

    feat: configure clipboard image.
    default, markdown, wiki, and TeX use unique affix operators.

--- lua/traap/plugins/clipboard-image.lua
+++ lua/traap/plugins/clipboard-image.lua
@@ -1,5 +1,31 @@
 return {
-  "ekickx/clipboard-image.nvim",
+  "dfendr/clipboard-image.nvim",
   event = { "BufReadPost", "BufNewFile" },
   enabled = true,
+
+  keys = {
+    {"<leader>pi", "<cmd>PasteImg<cr>", "Save and Past Image"}
+  },
+
+  config = function()
+    require("clipboard-image").setup({
+      default  = {
+        img_dir = {"images", "%:p:h:t", "%:t:r"},
+        img_dir_txt = {"images", "%:p:h:t", "%:t:r"},
+        img_name = function ()
+          vim.fn.inputsave()
+          local name = vim.fn.input('Name: ')
+          vim.fn.inputrestore()
+          if name == nil or name == '' then
+            return os.date('%y-%m-%d-%H-%M-%S')
+          end
+          return name
+        end,
+        affix  = "%s",
+      },
+      markdown = { affix = "![](%s)" },
+      tex      = { affix = "{\\includegraphics{%s}}" },
+      wiki     = { affix = "![](%s)" },
+    })
+  end,
 }
