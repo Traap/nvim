18db6b89ea92ea8c4503d295b7c7f6f9a6145ba3
Traap <gary.a.howard@mac.com>
2023-12-09 13:26:52 -0800

    Configure nvim-toggler using config() function.

--- lua/traap/plugins/nvim-toggler.lua
+++ lua/traap/plugins/nvim-toggler.lua
@@ -3,23 +3,21 @@ return {
   enabled = true,
   event = {"BufReadPost", "BufNewFile"},
 
-  init = function()
-    local toggler = require("nvim-toggler")
-    vim.keymap.set( {'n', 'v'},
-      '<leader>tn', toggler.toggle,
-      { noremap = true, silent = true, desc = "Toggle operand"}
-    )
-  end,
-
   config = function(_, opts)
     local toggler = require("nvim-toggler")
+
     toggler.setup({
       inverses = {
         [">="] = "<=",
         [">"] = "<",
       },
       remove_default_keybinds = true,
-      remvoe_default_inverses = false,
+      remove_default_inverses = false,
     })
+
+    vim.keymap.set( {'n', 'v'},
+      '<leader>tn', toggler.toggle,
+      { noremap = true, silent = true, desc = "Toggle operand"}
+    )
   end,
 }
