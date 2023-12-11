7a432efca32cfef925f75b3e3ee566987ff95fd5
Traap <gary.a.howard@mac.com>
2023-12-11 15:49:10 -0800

    wip - Multiple wiki support.

--- lua/traap/plugins/wiki-vim.lua
+++ lua/traap/plugins/wiki-vim.lua
@@ -13,11 +13,12 @@ return {
   keys = {
     {"<leader>wb", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Backlog.md<cr>", "Wiki Backlog"},
     {"<leader>wa", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Acronyms.md<cr>", "Wiki Acronyms"},
-    {"<leader>vc", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/ventura/index.md<cr>", "Ventua County"},
+    {"<leader>wk", "<cmd>edit " .. os.getenv("WORKHOME") .. "/wiki/index.md<cr>", "Work Wiki"},
+    {"<leader>yt", "<cmd>edit " .. os.getenv("YOUTUBEHOME") .. "/wiki/index.md<cr>", "YouTube Wiki"},
     {"<leader>we", "<cmd>WikiExport<cr>", "Wiki Export"},
     {"<leader>wi", "<cmd>WikiIndex<cr>", "Wiki Index"},
     {"<leader>wj", "<cmd>WikiJournal<cr>", "Wiki Journal"},
     {"<leader>wp", "<cmd>WikiFzfPages<cr>", "Wiki FzF Pages"},
     {"<leader>wv", "<cmd>lua Page_Viewer()<cr>"}
-  }
+  },
 }
