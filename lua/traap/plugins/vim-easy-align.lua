2c204f6be83c80a7dcf8244fd97d67ebfbfce2a6
Traap <gary.a.howard@mac.com>
2023-12-14 21:27:10 -0800

    Silence Noice when running EasyAign commands.

--- lua/traap/plugins/vim-easy-align.lua
+++ lua/traap/plugins/vim-easy-align.lua
@@ -4,11 +4,57 @@ return {
   enabled = true,
 
   config = function()
-    vim.cmd([[silent xmap ga <Plug>(EasyAlign)]])
-    vim.cmd([[silent nmap ga <Plug>(EasyAlign)]])
-    vim.cmd([[silent nmap <bar>     gaip*<bar>]])
-    vim.cmd([[silent nmap <leader>0 gaip*,]])
-    vim.cmd([[silent nmap <leader>1 gaip=,]])
-    vim.cmd([[silent nmap <leader>2 gaip=<space>]])
+
+    vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)', { desc = 'EasyAlign'})
+    vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)', { desc = 'EasyAlign'})
+
+    vim.keymap.set(
+      'n', '<bar>',
+      function()
+        vim.cmd([[NoiceDisable]])
+        vim.opt.cmdheight = 1
+        vim.cmd("normal gaip*|")
+        vim.opt.cmdheight = 0
+        vim.cmd([[NoiceEnable]])
+      end,
+      { desc = 'EasyAlign gaip*<bar>'}
+    )
+
+    vim.keymap.set(
+      'n', '<leader>0',
+      function()
+        vim.cmd([[NoiceDisable]])
+        vim.opt.cmdheight = 1
+        vim.cmd("normal gaip*,")
+        vim.opt.cmdheight = 0
+        vim.cmd([[NoiceEnable]])
+      end,
+      { desc = 'EasyAlign gaip=,'}
+    )
+
+    vim.keymap.set(
+      'n', '<leader>1',
+      function()
+        vim.cmd([[NoiceDisable]])
+        vim.opt.cmdheight = 1
+        vim.cmd("normal gaip=,")
+        vim.opt.cmdheight = 0
+        vim.cmd([[NoiceEnable]])
+      end,
+      { desc = 'EasyAlign gaip=,'}
+    )
+
+    vim.keymap.set(
+      'n', '<leader>2',
+      function()
+        vim.cmd([[NoiceDisable]])
+        vim.opt.cmdheight = 1
+        vim.cmd("normal gaip= ")
+        vim.opt.cmdheight = 0
+        vim.cmd([[NoiceDisable]])
+      end,
+      { desc = 'EasyAlign gaip=<space>'}
+    )
+
   end,
 }
