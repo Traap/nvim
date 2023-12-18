445649a51ba5c121a165a33f26bbfa477aa818d4
Traap <gary.a.howard@mac.com>
2023-12-17 20:41:46 -0800

    Setup lua lsp options.

--- lua/traap/lsp/servers.lua
+++ lua/traap/lsp/servers.lua
@@ -5,14 +5,17 @@ lsp_zero.on_attach(function(_, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require("mason").setup({})
-
-local Lsp = require("traap.lsp.opts")
-require("mason-lspconfig").setup(Lsp.opts)
-
--- Configure lua_ls
-local lua_opts = lsp_zero.nvim_lua_ls()
-require("lspconfig").lua_ls.setup(lua_opts)
+-- Setup Mason and Mason-Config.
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  },
+})
 
 -- Lsp popups use borders.
 require('lspconfig.ui.windows').default_options.border = 'single'
