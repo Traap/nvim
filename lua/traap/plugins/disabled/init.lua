5a70709621a2d1cb8e71f94bd7bbcd1141da3a6f
Traap <gary.a.howard@mac.com>
2023-12-24 11:05:16 -0800

    Back to lazy.vim step 2.

--- lua/traap/plugins/disabled/init.lua
+++ lua/traap/plugins/disabled/init.lua
@@ -1,8 +1,10 @@
 return {
   {"akinsho/bufferline.nvim", enabled = false},
-  {"stevearc/conform.nvim", enabled = false},
+  {"stevearc/dressing.nvim", enabled = false},
   {"echasnovski/mini.ai", enabled = false},
   {"echasnovski/mini.comment", enabled = false},
   {"echasnovski/mini.indentscope", enabled = false},
-  {"echasnovski/mini.pairs", enabled = false}
+  {"echasnovski/mini.pairs", enabled = false},
+  {"echasnovski/mini.surround", enabled = false},
+  {"RRethy/vim-illuminate", enabled = false}
 }
