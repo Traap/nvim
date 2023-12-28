01ee5da11f96f27b28666485d65a93499099a9db
Traap <gary.a.howard@mac.com>
2023-12-27 22:31:05 -0800

    Enable flit and leap. Disable flash

--- lua/traap/plugins/disabled/init.lua
+++ lua/traap/plugins/disabled/init.lua
@@ -1,6 +1,7 @@
 return {
   {"RRethy/vim-illuminate", enabled = false},
   {"akinsho/bufferline.nvim", enabled = false},
+  {"folke/flash.nvim", enabled = false},
   {"echasnovski/mini.ai", enabled = false},
   {"echasnovski/mini.comment", enabled = false},
   {"echasnovski/mini.indentscope", enabled = false},
---
+++ lua/traap/plugins/finder/flit.lua
@@ -0,0 +1,12 @@
+return {
+  "ggandor/flit.nvim",
+  enabled = true,
+  keys = function()
+    local ret = {}
+    for _, key in ipairs({ "f", "F", "t", "T" }) do
+      ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+    end
+    return ret
+  end,
+  opts = { labeled_modes = "nx" },
+}
