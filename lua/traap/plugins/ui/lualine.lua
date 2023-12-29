bcf5db29d354c53dd863006f905112518501e2e5
Traap <gary.a.howard@mac.com>
2023-12-28 19:54:15 -0800

    Use lazy.vim lualine.init() definitions.

--- lua/traap/plugins/ui/lualine.lua
+++ lua/traap/plugins/ui/lualine.lua
@@ -5,18 +5,6 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
 
-  -- See lazy.vim
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = " "
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
-  end,
-
   opts = function(_, opts)
     local function show_macro_recording()
       local recording_register = vim.fn.reg_recording()
