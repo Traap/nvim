30e86eb803612340325e14d4a1db21474284e15f
Traap <gary.a.howard@mac.com>
2024-01-02 06:52:06 -0800

    More work words.

--- lua/traap/plugins/wiki/markdown-preview.lua
+++ lua/traap/plugins/wiki/markdown-preview.lua
@@ -1,5 +1,11 @@
 return {
   "iamcco/markdown-preview.nvim",
-  ft = "md",
   enabled = true,
+  keys = {
+    {"<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Toggle Preview"},
+    {"<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview"},
+    {"<leader>ms", "<cmd>MarkdownPreviewStop<cr>", desc = "Markdown Stop Preview"},
+  },
+  ft = {"wiki", "markdown", "md", "puml"},
+  build = function() vim.fn["mkdp#util#install"]() end,
 }
