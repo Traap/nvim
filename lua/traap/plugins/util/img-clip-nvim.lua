ba27b345d1bc6a6593a87da90d6039322561fab8
Traap <gary.a.howard@mac.com>
2024-01-03 13:00:01 -0800

    Setup for tonight's testing.

--- lua/traap/plugins/util/img-clip-nvim.lua
+++ lua/traap/plugins/util/img-clip-nvim.lua
@@ -13,25 +13,25 @@ return {
       end,
 
       file_name = "%Y-%m-%d-%H-%M-%S",
-      url_encode_path = false,
+      url_encode_path = true,
       use_absolute_path = false,
 
       -- YouTube: 2nd - Toggle relative_to_current_file.
       relative_to_current_file = true,
-      prompt_for_file_name = true,
+      prompt_for_file_name = false,
 
       -- YouTube: 3rd - Toggle show_dir_path_in_prompt
       show_dir_path_in_prompt = true,
 
-      use_cursor_in_template = false,
+      use_cursor_in_template = true,
       insert_mode_after_paste = true,
-      embed_image_as_base64 = false,
+      embed_image_as_base64 = true,
       max_base64_size = 10,
       template = "$FILE_PATH",
 
       -- YouTube: 4th - Drag and drop
       drag_and_drop = {
-        enabled = false,
+        enabled = true,
         insert_mode = true,
         copy_images = true,
         download_images = true,
