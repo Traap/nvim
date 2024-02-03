e5dc95aea98c5210dd90fb9315847ebb5d185226
Traap <gary.a.howard@mac.com>
2024-02-03 14:23:36 -0800

    Define harpoon 2 dependencies.

--- lua/traap/plugins/harpoon.lua
+++ lua/traap/plugins/harpoon.lua
@@ -4,7 +4,10 @@ return {
   "ThePrimeagen/harpoon",
   enabled = true,
   branch = "harpoon2",
-  dependencies = {"nvim-lua/plenary.nvim"},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
 
   -- ----------------------------------------------------------------------- }}}
   -- {{{ Define events to load Harpoon.
