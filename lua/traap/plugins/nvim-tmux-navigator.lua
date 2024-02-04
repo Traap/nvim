0e64690f5dc6732a43d820116756a9a84bd5a5a1
Traap <gary.a.howard@mac.com>
2024-02-03 20:29:48 -0800

    Experiment with nvim-tmux-navigator.lua.

---
+++ lua/traap/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,21 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  enabled = true,
+  event = "VeryLazy",
+
+  opts = function(_, opts)
+    opts.disable_when_zoomed = true
+    opts.keybindings = {
+      left = '<c-h>',
+      down = '<c-j>',
+      up = '<c-k>',
+      right = '<c-l>',
+      last_active = '<c-\\>',
+      next='<c-]>',
+    }
+  end,
+
+  config = function(_, opts)
+    require("nvim-tmux-navigation").setup(opts)
+  end,
+}
