1a3009b2f3e5e4b9efe9a31138ec3cb0c5b5c92b
Traap <gary.a.howard@mac.com>
2024-03-02 08:22:19 -0800

    Disable lunarvim.breadcrumbs.

--- lua/traap/plugins/breadcrumbs.lua
+++ lua/traap/plugins/breadcrumbs.lua
@@ -1,7 +1,7 @@
 return {
   "LunarVim/breadcrumbs.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  enabled = true,
+  enabled = false,
   config = function()
     require("breadcrumbs").setup()
   end,
