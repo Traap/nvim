4a752d63f6c760ccc6a84736b3ffe6e7b32c6058
Traap <gary.a.howard@mac.com>
2024-04-20 15:14:36 -0700

    Experiment with new cmp completion on all systems.

--- lua/traap/plugins/cmp.lua
+++ lua/traap/plugins/cmp.lua
@@ -64,10 +64,10 @@ return {
     -- --------------------------------------------------------------------- }}}
     -- {{{ Has words before
 
-    -- local function has_words_before()
-    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    -- end
+    local function has_words_before()
+      local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+    end
 
     local check_backspace = function()
       local col = vim.fn.col "." - 1
@@ -103,16 +103,19 @@ return {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
+        -- elseif luasnip.expandable() then
+        --   luasnip.expand()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif check_backspace() then
           fallback()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
       end, { "i", "s" }),
+
 
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
