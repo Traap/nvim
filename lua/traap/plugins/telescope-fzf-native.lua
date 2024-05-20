756bdde5b6f54a1df8fe7e9aad314d59856d7745
Traap <gary.a.howard@mac.com>
2024-05-19 19:22:07 -0700

    fix: telescope-fzf-native dependencies

--- lua/traap/plugins/telescope-fzf-native.lua
+++ lua/traap/plugins/telescope-fzf-native.lua
@@ -1,9 +1,11 @@
 return {
   "nvim-telescope/telescope-fzf-native.nvim",
   enabled = true,
-  build = "make",
+
+  build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
 
   dependencies = {
+    "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
 
