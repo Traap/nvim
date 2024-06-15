9e8ceef4071145e3130747ce70f17ff2b0578e64
Traap <gary.a.howard@mac.com>
2024-06-14 23:35:21 -0700

    LuaSnip cleanup.

--- lua/traap/plugins/LuaSnip.lua
+++ lua/traap/plugins/LuaSnip.lua
@@ -1,11 +1,10 @@
 return {
-	"L3MON4D3/LuaSnip",
+  "L3MON4D3/LuaSnip",
   enabled = true,
   event = {"BufReadPost", "BufNewFile"},
   dependencies = {
     "hrsh7th/nvim-cmp",
     "saadparwaiz1/cmp_luasnip",
   },
-	version = "v2.*",
-	build = "make install_jsregexp"
+  build = "make install_jsregexp"
 }
