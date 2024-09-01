d6dc6c18994911307a0f5a962e636c055db79910
Traap <gary.a.howard@mac.com>
2024-09-01 12:04:21 -0700

    Change dadbod setup.

--- lua/traap/plugins/dadbod.lua
+++ lua/traap/plugins/dadbod.lua
@@ -1,9 +1,29 @@
 return {
-  "tpope/vim-dadbod",
-   lazy = false,
-   enabled = true,
-   dependencies = {
-    "kristijanhusak/vim-dadbod-completion",
-    "kristijanhusak/vim-dadbod-ui",
-  }
+  "kristijanhusak/vim-dadbod-ui",
+
+  enabled = true,
+
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion",
+      ft = { "sql", "mysql", "plsql" }, lazy = true
+    }, -- Optional
+  },
+
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_auto_execute_table_helpers = 1
+    vim.g.db_ui_table_helpers = {
+      sqlserver = {
+        Count = "select count(*) from {table}"
+      }
+    }
+  end,
 }
