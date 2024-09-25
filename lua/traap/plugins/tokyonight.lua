3e1d634677f6bf9b607057d92642271695b58739
Traap <gary.a.howard@mac.com>
2024-09-24 23:16:34 -0700

    Set tokyonight -night to transparent.

--- lua/traap/plugins/tokyonight.lua
+++ lua/traap/plugins/tokyonight.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000,
 
    opts = function(_, opts)
-    opts.transparent = false
+    opts.transparent = true
     opts.style = "night"
     opts.styles = {
       comments = { italic = true },
