8fed3ff22b5a4dcc5d06920f7ba50a4880032dd0
Traap <gary.a.howard@mac.com>
2024-12-02 07:55:00 -0800

    Add key function to wiki-vim.

--- lua/traap/plugins/wiki-vim.lua
+++ lua/traap/plugins/wiki-vim.lua
@@ -5,16 +5,61 @@ return {
   cmd = { "WikiIndex", "WikiJournal" },
   ft = { "wiki", "md" },
 
-  keys = {
-    {"<leader>pw", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/index.md<cr>", desc = "Personal Wiki"},
-    {"<leader>wb", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Backlog.md<cr>", desc = "Wiki Backlog"},
-    {"<leader>wa", "<cmd>edit " .. os.getenv("WIKIHOME") .. "/journal/Acronyms.md<cr>", desc = "Wiki Acronyms"},
-    {"<leader>wk", "<cmd>edit " .. os.getenv("WORKHOME") .. "/Wiki/index.md<cr>", desc = "Work Wiki"},
-    {"<leader>yt", "<cmd>edit " .. os.getenv("YOUTUBEHOME") .. "/wiki/index.md<cr>", desc = "YouTube Wiki"},
-    {"<leader>we", "<cmd>WikiExport<cr>", desc = "Wiki Export"},
-    {"<leader>wi", "<cmd>WikiIndex<cr>", desc = "Wiki Index"},
-    {"<leader>wj", "<cmd>WikiJournal<cr>", desc = "Wiki Journal"},
-    {"<leader>wp", "<cmd>WikiFzfPages<cr>", desc = "Wiki FzF Pages"},
-    {"<leader>wv", "<cmd>lua Page_Viewer()<cr>", desc = "Wiki View Exported Page" }
-  },
+  keys = function()
+    local wikihome = os.getenv("WIKIHOME")
+    local workhome = os.getenv("WORKHOME")
+    local   ythome = os.getenv("YOUTUBEHOME")
+    local keys = {
+      {"<leader>pw",
+        "<cmd>edit " .. wikihome .. "/index.md<cr>",
+        desc = "Personal Wiki"
+      },
+
+      {"<leader>wb",
+        "<cmd>edit " ..  wikihome .. "/journal/Backlog.md<cr>",
+        desc = "Wiki Backlog"
+      },
+
+      {"<leader>wa",
+        "<cmd>edit " .. wikihome .. "/journal/Acronyms.md<cr>",
+        desc = "Wiki Acronyms"
+      },
+
+      {"<leader>wk",
+        "<cmd>edit " .. workhome .. "/Wiki/index.md<cr>",
+        desc = "Work Wiki"
+      },
+
+      {"<leader>yt",
+        "<cmd>edit " .. ythome .. "/wiki/index.md<cr>",
+        desc = "YouTube Wiki"
+      },
+
+      {"<leader>we",
+        "<cmd>WikiExport<cr>",
+        desc = "Wiki Export"
+      },
+
+      {"<leader>wi",
+        "<cmd>edit " .. wikihome .. "/index.md<cr>",
+        desc = "Personal Wiki"
+      },
+
+      {"<leader>wj",
+        "<cmd>cd ".. wikihome .."<cr><cmd>WikiJournal<cr>",
+        desc = "Wiki Journal"
+      },
+
+      {"<leader>wp",
+        "<cmd>WikiFzfPages<cr>",
+        desc = "Wiki FzF Pages"
+      },
+
+      {"<leader>wv",
+        "<cmd>lua Page_Viewer()<cr>",
+        desc = "Wiki View Exported Page"
+      }
+    }
+    return keys
+  end,
 }
