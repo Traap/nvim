795a66230e4fe03b3caa33598e9f932ebdbc9853
Traap <gary.a.howard@mac.com>
2024-12-31 18:48:25 -0800

    Telescope add find packages and config keybindings.

--- lua/traap/plugins/telescope.lua
+++ lua/traap/plugins/telescope.lua
@@ -13,7 +13,6 @@ return {
 
   keys = function()
     local cmdT = "<cmd>Telescope "
-    local cmdL = "<cmd>lua require('telescope')."
     return {
       {"<leader>fC", cmdT .. "commands<cr>", desc = "Telescope commands" },
       {"<leader>fF", cmdT .. "media_files<cr>", desc = "Telescope media files" },
@@ -22,17 +21,33 @@ return {
       {"<leader>fS", cmdT .. "colorscheme<cr>", desc = "Telescope colorschemes" },
 
       {"<leader>fb", cmdT .. "buffers<cr>", desc = "Telescope buffers" },
+
+      {"<leader>fc",
+       function()
+         require("telescope.builtin").find_files{
+           cwd = vim.fn.stdpath("config")
+         }
+       end
+      },
+
       {"<leader>fd", cmdT .. "diagnostics<cr>", desc = "Telescope diagnostics" },
       {"<leader>ff", cmdT .. "find_files<cr>", desc = "Telescope Find files" },
       {"<leader>fg", cmdT .. "live_grep<cr>", desc = "Telescope Live Grep" },
       {"<leader>fh", cmdT .. "help_tags<cr>", desc = "Telecope Help files" },
 
-      {"<leader>fi", cmdL .. "extensions.media_files.media_files()<cr>", desc = "Telescope Media files" },
       {"<leader>fk", cmdT .. "keymaps<cr>", desc = "Telescope keymaps" },
       {"<leader>fl", cmdT .. "resume<cr>", desc = "Telescope resume" },
       {"<leader>fm", cmdT .. "marks<cr>", desc = "Telescope marks" },
       {"<leader>fo", cmdT .. "oldfiles<cr>", desc = "Telescope old files" },
-      {"<leader>fp", cmdT .. "planets<cr>", desc = "Telescope Planets" },
+
+      {"<leader>fp",
+       function()
+         require("telescope.builtin").find_files{
+           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+         }
+       end
+      },
+
       {"<leader>fw", cmdT .. "grep_string<cr>", desc = "Telescope Find Word" },
 
       {"<leader>gC", cmdT .. "git_commits<cr>", desc = "Telescope git commits" },
