4a0574dbe1c3bbc5fe99e71e72db2929b9498bca
Traap <gary.a.howard@mac.com>
2025-01-26 17:56:26 -0800

    Change telescope key map description.

--- lua/traap/plugins/telescope.lua
+++ lua/traap/plugins/telescope.lua
@@ -14,48 +14,50 @@ return {
   keys = function()
     local cmdT = "<cmd>Telescope "
     return {
-      {"<leader>fC", cmdT .. "commands<cr>", desc = "Telescope commands" },
-      {"<leader>fF", cmdT .. "media_files<cr>", desc = "Telescope media files" },
-      {"<leader>fM", cmdT .. "man_pages<cr>", desc = "Telescope man pages" },
-      {"<leader>fR", cmdT .. "registers<cr>", desc = "Telescope registers" },
-      {"<leader>fS", cmdT .. "colorscheme<cr>", desc = "Telescope colorschemes" },
+      {"<leader>fC", cmdT .. "commands<cr>", desc = "List commands" },
+      {"<leader>fF", cmdT .. "media_files<cr>", desc = "Find media files" },
+      {"<leader>fM", cmdT .. "man_pages<cr>", desc = "Find man pages" },
+      {"<leader>fR", cmdT .. "registers<cr>", desc = "List registers" },
+      {"<leader>fS", cmdT .. "colorscheme<cr>", desc = "List colorschemes" },
 
-      {"<leader>fb", cmdT .. "buffers<cr>", desc = "Telescope buffers" },
+      {"<leader>fb", cmdT .. "buffers<cr>", desc = "List buffers" },
 
       {"<leader>fc",
        function()
          require("telescope.builtin").find_files{
            cwd = vim.fn.stdpath("config")
          }
-       end
+       end,
+       desc = "Neovim config files"
       },
 
-      {"<leader>fd", cmdT .. "diagnostics<cr>", desc = "Telescope diagnostics" },
-      {"<leader>ff", cmdT .. "find_files<cr>", desc = "Telescope Find files" },
-      {"<leader>fg", cmdT .. "live_grep<cr>", desc = "Telescope Live Grep" },
-      {"<leader>fh", cmdT .. "help_tags<cr>", desc = "Telecope Help files" },
+      {"<leader>fd", cmdT .. "diagnostics<cr>", desc = "Buffer diagnostics" },
+      {"<leader>ff", cmdT .. "find_files<cr>", desc = "Find Find files" },
+      {"<leader>fg", cmdT .. "live_grep<cr>", desc = "Live Grep" },
+      {"<leader>fh", cmdT .. "help_tags<cr>", desc = "List Help files" },
 
-      {"<leader>fk", cmdT .. "keymaps<cr>", desc = "Telescope keymaps" },
+      {"<leader>fk", cmdT .. "keymaps<cr>", desc = "List keymaps" },
       {"<leader>fl", cmdT .. "resume<cr>", desc = "Telescope resume" },
-      {"<leader>fm", cmdT .. "marks<cr>", desc = "Telescope marks" },
-      {"<leader>fo", cmdT .. "oldfiles<cr>", desc = "Telescope old files" },
+      {"<leader>fm", cmdT .. "marks<cr>", desc = "List marks" },
+      {"<leader>fo", cmdT .. "oldfiles<cr>", desc = "List old files" },
 
       {"<leader>fp",
        function()
          require("telescope.builtin").find_files{
            cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
          }
-       end
+       end,
+       desc = "Neovim plugin packages"
       },
 
-      {"<leader>fw", cmdT .. "grep_string<cr>", desc = "Telescope Find Word" },
+      {"<leader>fw", cmdT .. "grep_string<cr>", desc = "Find word under cursor" },
 
-      {"<leader>gC", cmdT .. "git_commits<cr>", desc = "Telescope git commits" },
-      {"<leader>gb", cmdT .. "git_branches<cr>", desc = "Telescope git branches" },
-      {"<leader>go", cmdT .. "git_status<cr>", desc = "Telescope git status" },
+      {"<leader>gC", cmdT .. "git_commits<cr>", desc = "List git commits" },
+      {"<leader>gb", cmdT .. "git_branches<cr>", desc = "List git branches" },
+      {"<leader>go", cmdT .. "git_status<cr>", desc = "List git status" },
 
-      {"<leader>LS", cmdT .. "lsp_dynamic_workspace_symbols<cr>", desc = "Telescope Workspace Symbols" },
-      {"<leader>Ls", cmdT .. "lsp_document_symbols<cr>", desc = "Telescope Document Symbols" },
+      {"<leader>LS", cmdT .. "lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols" },
+      {"<leader>Ls", cmdT .. "lsp_document_symbols<cr>", desc = "Document Symbols" },
     }
   end,
 
