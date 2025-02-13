495effc463c8bb4f334081a9bd319c03bc012a9e
Traap <gary.a.howard@mac.com>
2025-02-13 01:19:17 -0800

    Return of the question mark icon "?" for help.

--- lua/traap/plugins/noice.lua
+++ lua/traap/plugins/noice.lua
@@ -27,7 +27,7 @@ return {
         search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
         filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
         lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-        help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
         input = {},
       },
     }
@@ -44,7 +44,7 @@ return {
     }
 
     -- --------------------------------------------------------------------- }}}
-    -- {{{ LSP
+    -- {{{ LS󰋖P
 
     opts.lsp = {
       progress = {
