98202a1237d4bb2adec8d31400d8fd0fd9dde456
Traap <gary.a.howard@mac.com>
2025-02-12 23:17:33 -0800

    Change noice search icons.

--- lua/traap/plugins/noice.lua
+++ lua/traap/plugins/noice.lua
@@ -29,8 +29,8 @@ return {
       },
       format = {
         cmdline = { pattern = "^:", icon = "$", lang = "vim" },
-        search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+        search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+        search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
         filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
         lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
         help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
