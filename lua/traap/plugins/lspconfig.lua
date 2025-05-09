3e0e50d090d971edc1af96ae4af0c3d39825bfb9
Traap <gary.a.howard@mac.com>
2025-05-09 13:08:45 -0700

    Automatically install all lsp servers at first startup.

--- lua/traap/plugins/lspconfig.lua
+++ lua/traap/plugins/lspconfig.lua
@@ -1,11 +1,9 @@
 return {
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    ft = vim.tbl_keys(require('traap.config.servers').filetype_to_server),
-    -- ft = vim.tbl_keys(require('traap.config.servers').filetypes_for_lsp_servers()),
-    lazy = true,
-    -- {{{ Dependencies
+  'neovim/nvim-lspconfig',
+  event = { 'BufReadPre', 'BufNewFile' },
+  lazy = true,
+  enabled = true,
+  -- {{{ Dependencies
 
     dependencies = {
       {
@@ -19,112 +17,54 @@ return {
       },
       {
         'williamboman/mason.nvim',
-        config = true,
-        opts = { ui = { border = 'rounded' }, },
+        event = 'VimEnter',
+        opts = { ui = { border = 'rounded' } },
+        config = function(_, opts)
+          require('mason').setup(opts)
+        end,
       },
       {
         'williamboman/mason-lspconfig.nvim',
-        config = true,
+        event = 'VimEnter',
+        dependencies = { 'mason.nvim' },
+        -- We'll configure handlers below in one place
       },
     },
 
-    -- --------------------------------------------------------------------- }}}
+  -- ----------------------------------------------------------------------- }}}
+  -- {{{ Custom configuration.
+
     config = function()
-      -- {{{ local definitions
+      local servers = require('traap.config.servers').lsp_server_names()
+      local notify  = require('traap.core.notify')
 
-      local servers = require('traap.config.servers')
-      local notify = require('traap.core.notify')
-      local msg_header = '[traap] '
-      local debug = false
-
-      -- ------------------------------------------------------------------- }}}
-      -- {{{ Helper for debug notifications
-
-      local function notify_info(msg)
-        if debug then
-          notify.info(msg_header .. msg)
-        end
-      end
-
-      -- ------------------------------------------------------------------- }}}
-      -- {{{ LSP capabilities.
-
+      -- Common capabilities and on_attach for all servers
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-      -- ------------------------------------------------------------------- }}}
-      -- {{{ LSP on attached
-
       local on_attach = function(client, bufnr)
-        notify_info(client.name .. ' attached to buffer ' .. bufnr)
-
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        notify.info('[traap] ' .. client.name .. ' attached to buffer ' .. bufnr)
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', 'K',  vim.lsp.buf.hover,      bufopts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,      bufopts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
       end
 
-      -- ------------------------------------------------------------------- }}}
-      -- {{{ Autocommand for FileType
-
-      vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('traap-lsp-file-types', { clear = true }),
-        pattern = servers.filetypes_for_lsp_servers(),
-
-        callback = function(event)
-          local server = servers.filetype_to_server[event.match]
-          if not server then return end
-
-          notify_info('Server name: ' .. server.name)
-          notify_info('Event match: ' .. event.match)
-
-          require('mason').setup()
-          require('mason-lspconfig').setup {
-            ensure_installed = { server.name },
-            automatic_installation = true,
-          }
-
-          require('mason-lspconfig').setup_handlers {
-            [server.name] = function()
-              require('lspconfig')[server.name].setup {
-                on_attach = on_attach,
-                capabilities = capabilities,
-              }
-            end,
-          }
-        end,
-      })
-
-      -- ------------------------------------------------------------------- }}}
-      -- {{{ Autocommand for BufEnter
-
-      vim.api.nvim_create_autocmd('BufEnter', {
-        callback = function(event)
-          local buf = event.buf
-          local ft = vim.bo[buf].filetype
-          local server = servers.filetype_to_server[ft]
-          if not server then return end
-
-          local attached = false
-
-          for _, client in ipairs(vim.lsp.get_clients({ bufnr = buf })) do
-            if client.name == server.name then
-              attached = true
-              break
-            end
-          end
-
-          if not attached then
-            vim.defer_fn(function()
-              notify_info(server.name .. ' attaching to buffer ' .. buf)
-              vim.cmd('LspStart ' .. server.name)
-            end, 100)
-          end
-        end,
-      })
-
-      -- ------------------------------------------------------------------- }}}
+      -- Setup Mason + Mason-LSPConfig in one call
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        ensure_installed      = servers,
+        automatic_installation = true,
+        handlers = {
+          -- Default handler for all servers
+          function(server_name)
+            require('lspconfig')[server_name].setup {
+              on_attach    = on_attach,
+              capabilities = capabilities,
+            }
+          end,
+        },
+      }
     end,
-  },
+  -- ----------------------------------------------------------------------- }}}
 }
