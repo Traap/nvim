893c8e8ffa290a42bdb86071ca02dba186b4ea45
Traap <gary.a.howard@mac.com>
2025-09-22 20:02:15 -0700

    Really.  Fugitive uses cmd and key events.

--- lua/traap/plugins/fugitive.lua
+++ lua/traap/plugins/fugitive.lua
@@ -1,10 +1,3 @@
-local EVENT = nil
-if pcall(require, "lazyvim") then
-  EVENT = 'VeryLazy'
-else
-  EVENT = { "BufReadPost", "BufNewFile", "BufWritePre" }
-end
-
 return {
   "tpope/vim-fugitive",
   dependencies = {
@@ -12,17 +5,16 @@ return {
   },
 
   enabled = true,
-  event = EVENT,
   cmd = "G",
 
   keys = {
-    {"<leader>gP", "<cmd>G pull<cr>"},
-    {"<leader>gd", "<cmd>G diff<cr>"},
-    {"<leader>gl", "<cmd>G log<cr>"},
-    {"<leader>gh", "<cmd>vert bo help fugitive<cr>"},
-    {"<leader>gp", "<cmd>G push<cr>"},
-    {"<leader>gs", "<cmd>G<cr>"},
-    {"gh", "<cmd>diffget //2<cr>"}, -- Chose left side,
-    {"gl", "<cmd>diffget //3<cr>"}, -- Chose right size
+    { "<leader>gP", "<cmd>G pull<cr>" },
+    { "<leader>gd", "<cmd>G diff<cr>" },
+    { "<leader>gl", "<cmd>G log<cr>" },
+    { "<leader>gh", "<cmd>vert bo help fugitive<cr>" },
+    { "<leader>gp", "<cmd>G push<cr>" },
+    { "<leader>gs", "<cmd>G<cr>" },
+    { "gh",         "<cmd>diffget //2<cr>" }, -- Chose left side,
+    { "gl",         "<cmd>diffget //3<cr>" }, -- Chose right size
   },
 }
