a2b2ddec6b977d7cb2affccf0b3cc1b9ad7a2398
Traap <gary.a.howard@mac.com>
2025-10-12 14:09:46 -0700

    ctl-k changed to leader-k
    Remember: ctl-h,j,k,l are reserved to Vim split and TMUX pain movement.

--- lua/traap/plugins/lspconfig.lua
+++ lua/traap/plugins/lspconfig.lua
@@ -50,7 +50,7 @@ return {
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
         vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
         vim.keymap.set("n", "<leader>wl", function()
