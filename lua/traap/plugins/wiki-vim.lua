4e776d1de3de2aae8770d341745cbeec548a8d20
Traap <gary.a.howard@mac.com>
2026-02-26 08:23:41 -0800

    Disable vimtex and wiki-vim from vscode.
    NOTE: This plugs seam to work from vscode, archlinux, and neovim but now
    when vscode is run from WSL2/archlinux.  Environment variables are not
    found.

--- lua/traap/plugins/wiki-vim.lua
+++ lua/traap/plugins/wiki-vim.lua
@@ -1,6 +1,6 @@
 return {
   "lervag/wiki.vim",
-  enabled = true,
+  enabled = true and not vim.g.vscode,
 
   cmd = { "WikiIndex", "WikiJournal" },
   ft = { "wiki", "md" },
@@ -8,54 +8,64 @@ return {
   keys = function()
     local wikihome = os.getenv("WIKIHOME")
     local workhome = os.getenv("WORKHOME")
-    local   ythome = os.getenv("YOUTUBEHOME")
+    local ythome = os.getenv("YOUTUBEHOME")
     local keys = {
-      {"<leader>pw",
+      {
+        "<leader>pw",
         "<cmd>edit " .. wikihome .. "/index.md<cr>",
         desc = "Personal Wiki"
       },
 
-      {"<leader>wb",
-        "<cmd>edit " ..  wikihome .. "/journal/Backlog.md<cr>",
+      {
+        "<leader>wb",
+        "<cmd>edit " .. wikihome .. "/journal/Backlog.md<cr>",
         desc = "Wiki Backlog"
       },
 
-      {"<leader>wa",
+      {
+        "<leader>wa",
         "<cmd>edit " .. wikihome .. "/journal/Acronyms.md<cr>",
         desc = "Wiki Acronyms"
       },
 
-      {"<leader>wk",
+      {
+        "<leader>wk",
         "<cmd>edit " .. workhome .. "/Wiki/index.md<cr>",
         desc = "Work Wiki"
       },
 
-      {"<leader>yt",
+      {
+        "<leader>yt",
         "<cmd>edit " .. ythome .. "/wiki/index.md<cr>",
         desc = "YouTube Wiki"
       },
 
-      {"<leader>we",
+      {
+        "<leader>we",
         "<cmd>WikiExport<cr>",
         desc = "Wiki Export"
       },
 
-      {"<leader>wi",
+      {
+        "<leader>wi",
         "<cmd>edit " .. wikihome .. "/index.md<cr>",
         desc = "Personal Wiki"
       },
 
-      {"<leader>wj",
-        "<cmd>cd ".. wikihome .."<cr><cmd>WikiJournal<cr>",
+      {
+        "<leader>wj",
+        "<cmd>cd " .. wikihome .. "<cr><cmd>WikiJournal<cr>",
         desc = "Wiki Journal"
       },
 
-      {"<leader>wp",
+      {
+        "<leader>wp",
         "<cmd>WikiFzfPages<cr>",
         desc = "Wiki FzF Pages"
       },
 
-      {"<leader>wv",
+      {
+        "<leader>wv",
         "<cmd>lua Page_Viewer()<cr>",
         desc = "Wiki View Exported Page"
       }
