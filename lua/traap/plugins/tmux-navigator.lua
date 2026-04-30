9c6c35efb8c1061dabb41b3b6dcb3bf66489a869
Traap <gary.a.howard@mac.com>
2026-04-30 11:26:34 -0700

    Experiment with TMUX zoom flags.

--- lua/traap/plugins/tmux-navigator.lua
+++ lua/traap/plugins/tmux-navigator.lua
@@ -2,4 +2,8 @@ local platform = require("traap.core.platform")
 return {
   "christoomey/vim-tmux-navigator",
   enabled = true and platform.is_nvim(),
+  init = function()
+    vim.g.tmux_navigator_disable_when_zoomed = 1
+    vim.g.tmux_navigator_preserve_zoom = 1
+  end,
 }
