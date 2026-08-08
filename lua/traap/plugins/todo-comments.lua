5a4af07bd23989f448ff08b591d26d5c73f959b6
Traap <gary.a.howard@mac.com>
2026-08-08 11:57:53 -0700

    Fixed todo-comments syntax error.

--- lua/traap/plugins/todo-comments.lua
+++ lua/traap/plugins/todo-comments.lua
@@ -6,10 +6,13 @@ return {
   opts = function(_, opts)
     opts.merge_keywords = true
     opts.keywords = {
-      YouTube = { icon = " ", color = "#ff0000", alt = { "youtube", "Youtube" } },
+      YouTube = {
+        icon = " ",
+        color = "#ff0000",
+        alt = { "youtube", "Youtube" },
+      },
       URL = { icon = " ", color = "#7711FF", alt = { "Url", "url" } },
     }
-    opts.higlight = { comments_only = false, }
+    opts.highlight = { comments_only = false }
   end,
-
 }
